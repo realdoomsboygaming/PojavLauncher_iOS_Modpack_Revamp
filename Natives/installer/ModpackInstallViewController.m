70236052b709e7abd60d4dddb5ba55ed4331f623
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-17 18:28:19 -0600

    Update ModpackInstallViewController.m

--- Natives/installer/ModpackInstallViewController.m
+++ Natives/installer/ModpackInstallViewController.m
@@ -4,103 +4,117 @@
 #import "UIKit+AFNetworking.h"
 #import "UIKit+hook.h"
 #import "WFWorkflowProgressView.h"
+#import "modpack/ModrinthAPI.h"
 #import "config.h"
-#import "CurseforgeAPI.h"
-#import "ModrinthAPI.h"
 #import "ios_uikit_bridge.h"
 #import "utils.h"
 #include <dlfcn.h>
 
-@interface ModpackInstallViewController () <UISearchResultsUpdating>
+#define kCurseForgeGameIDMinecraft 432
+#define kCurseForgeClassIDModpack 4471
+#define kCurseForgeClassIDMod 6
 
-@property (nonatomic, strong) UISearchController *searchController;
-@property (nonatomic, strong) NSMutableArray *list;
-@property (nonatomic, strong) NSMutableDictionary *filters;
-
-@property (nonatomic, strong) ModrinthAPI *modrinth;  // ModrinthAPI
-@property (nonatomic, strong) CurseforgeAPI *curseforge;  // CurseForgeAPI
-
-@property (nonatomic, assign) BOOL isUsingCurseForge;  // Boolean to track which API is active
-
+@interface ModpackInstallViewController()<UIContextMenuInteractionDelegate>
+@property(nonatomic) UISearchController *searchController;
+@property(nonatomic) UIMenu *currentMenu;
+@property(nonatomic) NSMutableArray *list;
+@property(nonatomic) NSMutableDictionary *filters;
+@property ModrinthAPI *modrinth;
+@property (nonatomic, strong) NSString *selectedAPI; // Added to switch between APIs
 @end
 
 @implementation ModpackInstallViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.view.backgroundColor = [UIColor systemBackgroundColor];
-    self.tableView.backgroundColor = [UIColor systemGroupedBackgroundColor];
-    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    
-    self.refreshControl = [[UIRefreshControl alloc] init];
-    [self.refreshControl addTarget:self action:@selector(refreshSearchResults) forControlEvents:UIControlEventValueChanged];
-    
+
+    self.selectedAPI = @"Modrinth"; // Default to Modrinth
     self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
     self.searchController.searchResultsUpdater = self;
     self.searchController.obscuresBackgroundDuringPresentation = NO;
-    self.searchController.searchBar.placeholder = @"Search Modpacks";
     self.navigationItem.searchController = self.searchController;
-    
-    // Initialize both APIs
+
     self.modrinth = [ModrinthAPI new];
-    self.curseforge = [[CurseforgeAPI alloc] initWithApiKey:@"your_api_key_here"];  // Initialize with API key
-    
-    // Default API to use
-    self.isUsingCurseForge = NO;  // Initially using Modrinth
-    
-    self.filters = [@{ @"isModpack": @(YES), @"name": @" " } mutableCopy];
-    
-    // Add a Segmented Control to switch between APIs
-    [self addAPISelectionControl];
-    
+    self.filters = @{
+        @"isModpack": @(YES),
+        @"name": @" "
+    }.mutableCopy;
     [self updateSearchResults];
-}
-
-- (void)addAPISelectionControl {
-    UISegmentedControl *apiSwitch = [[UISegmentedControl alloc] initWithItems:@[@"Modrinth", @"CurseForge"]];
-    apiSwitch.selectedSegmentIndex = self.isUsingCurseForge ? 1 : 0;
-    [apiSwitch addTarget:self action:@selector(apiSwitchChanged:) forControlEvents:UIControlEventValueChanged];
-    self.navigationItem.titleView = apiSwitch;
-}
-
-- (void)apiSwitchChanged:(UISegmentedControl *)sender {
-    self.isUsingCurseForge = sender.selectedSegmentIndex == 1;
-    [self updateSearchResults];  // Update the search results when the API is switched
-}
-
-- (void)refreshSearchResults {
-    [self loadSearchResultsWithPrevList:NO];
 }
 
 - (void)loadSearchResultsWithPrevList:(BOOL)prevList {
     NSString *name = self.searchController.searchBar.text;
-    if (!prevList && [self.filters[@"name"] isEqualToString:name]) return;
-    
+    if (!prevList && [self.filters[@"name"] isEqualToString:name]) {
+        return;
+    }
+
     [self switchToLoadingState];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         self.filters[@"name"] = name;
-        
-        // Use the correct API based on the selection
-        if (self.isUsingCurseForge) {
-            self.list = [self.curseforge searchModWithFilters:self.filters previousPageResult:(prevList ? self.list : nil)];
-        } else {
-            self.list = [self.modrinth searchModWithFilters:self.filters previousPageResult:(prevList ? self.list : nil)];
+
+        if ([self.selectedAPI isEqualToString:@"Modrinth"]) {
+            self.list = [self.modrinth searchModWithFilters:self.filters previousPageResult:prevList ? self.list : nil];
+        } else if ([self.selectedAPI isEqualToString:@"CurseForge"]) {
+            [self fetchCurseForgeModpacksWithName:name prevList:prevList];
+            return;
         }
-        
+
         dispatch_async(dispatch_get_main_queue(), ^{
             if (self.list) {
                 [self switchToReadyState];
                 [self.tableView reloadData];
             } else {
-                showDialog(@"Error", self.isUsingCurseForge ? self.curseforge.lastError.localizedDescription : self.modrinth.lastError.localizedDescription);
+                showDialog(localize(@"Error", nil), self.modrinth.lastError.localizedDescription);
                 [self actionClose];
-            }
-            if (self.refreshControl.isRefreshing) {
-                [self.refreshControl endRefreshing];
             }
         });
     });
+}
+
+- (void)fetchCurseForgeModpacksWithName:(NSString *)name prevList:(BOOL)prevList {
+    // Use the CurseForge API to search for modpacks
+    NSString *urlString = [NSString stringWithFormat:@"https://api.curseforge.com/v1/mods/search?gameId=%@&classId=%@&search=%@", @(kCurseForgeGameIDMinecraft), @(kCurseForgeClassIDModpack), name];
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setValue:@"Bearer YOUR_API_KEY" forHTTPHeaderField:@"Authorization"]; // Set your CurseForge API key here
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                showDialog(localize(@"Error", nil), error.localizedDescription);
+                [self actionClose];
+            });
+            return;
+        }
+
+        NSError *jsonError;
+        NSDictionary *jsonResponse = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        
+        if (jsonError) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                showDialog(localize(@"Error", nil), jsonError.localizedDescription);
+                [self actionClose];
+            });
+            return;
+        }
+
+        NSArray *modpacks = jsonResponse[@"data"];
+        self.list = [modpacks mutableCopy];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (self.list) {
+                [self switchToReadyState];
+                [self.tableView reloadData];
+            } else {
+                showDialog(localize(@"Error", nil), @"No modpacks found.");
+                [self actionClose];
+            }
+        });
+    }];
+    
+    [dataTask resume];
 }
 
 - (void)updateSearchResults {
@@ -112,10 +126,6 @@
     [self performSelector:@selector(updateSearchResults) withObject:nil afterDelay:0.5];
 }
 
-- (void)actionClose {
-    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
-}
-
 - (void)switchToLoadingState {
     UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleMedium];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:indicator];
@@ -125,9 +135,24 @@
 }
 
 - (void)switchToReadyState {
+    UIActivityIndicatorView *indicator = (id)self.navigationItem.rightBarButtonItem.customView;
+    [indicator stopAnimating];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemClose target:self action:@selector(actionClose)];
     self.navigationController.modalInPresentation = NO;
     self.tableView.allowsSelection = YES;
+}
+
+#pragma mark UIContextMenu
+
+- (UIContextMenuConfiguration *)contextMenuInteraction:(UIContextMenuInteraction *)interaction configurationForMenuAtLocation:(CGPoint)location
+{
+    return [UIContextMenuConfiguration configurationWithIdentifier:nil previewProvider:nil actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions) {
+        return self.currentMenu;
+    }];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -135,88 +160,20 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ModpackCell"];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"ModpackCell"];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"];
+        cell.imageView.contentMode = UIViewContentModeScaleToFill;
+        cell.imageView.clipsToBounds = YES;
     }
-    
+
     NSDictionary *item = self.list[indexPath.row];
-    cell.textLabel.text = item[@"title"];
+    cell.textLabel.text = item[@"name"] ?: item[@"title"];
     cell.detailTextLabel.text = item[@"description"];
-    [cell.imageView setImageWithURL:[NSURL URLWithString:item[@"imageUrl"]] placeholderImage:[UIImage imageNamed:@"DefaultProfile"]];
-    
+    UIImage *fallbackImage = [UIImage imageNamed:@"DefaultProfile"];
+    [cell.imageView setImageWithURL:[NSURL URLWithString:item[@"imageUrl"]] placeholderImage:fallbackImage];
+
     return cell;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSDictionary *item = self.list[indexPath.row];
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    
-    [self switchToLoadingState];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        if (self.isUsingCurseForge) {
-            [self.curseforge loadModDetails:item completion:^(NSDictionary *modDetails) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [self switchToReadyState];
-                    if (modDetails) {
-                        [self presentVersionOptionsForItem:item atIndexPath:indexPath modDetails:modDetails];
-                    } else {
-                        showDialog(@"Error", self.curseforge.lastError.localizedDescription);
-                    }
-                });
-            }];
-        } else {
-            [self.modrinth loadModDetails:item completion:^(NSDictionary *modDetails) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [self switchToReadyState];
-                    if (modDetails) {
-                        [self presentVersionOptionsForItem:item atIndexPath:indexPath modDetails:modDetails];
-                    } else {
-                        showDialog(@"Error", self.modrinth.lastError.localizedDescription);
-                    }
-                });
-            }];
-        }
-    });
-}
-
-- (void)presentVersionOptionsForItem:(NSDictionary *)item atIndexPath:(NSIndexPath *)indexPath modDetails:(NSDictionary *)modDetails {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:item[@"title"] message:@"Select a version to install" preferredStyle:UIAlertControllerStyleActionSheet];
-    
-    NSArray *versionNames = modDetails[@"versionNames"];
-    for (NSString *versionName in versionNames) {
-        UIAlertAction *action = [UIAlertAction actionWithTitle:versionName style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-            [self actionClose];
-            if (self.isUsingCurseForge) {
-                [self.curseforge downloadModpack:item atVersion:versionName completion:^(BOOL success, NSError *error) {
-                    if (success) {
-                        // Handle success
-                    } else {
-                        // Handle error
-                        showDialog(@"Error", error.localizedDescription);
-                    }
-                }];
-            } else {
-                [self.modrinth downloadModpack:item atVersion:versionName completion:^(BOOL success, NSError *error) {
-                    if (success) {
-                        // Handle success
-                    } else {
-                        // Handle error
-                        showDialog(@"Error", error.localizedDescription);
-                    }
-                }];
-            }
-        }];
-        [alert addAction:action];
-    }
-    
-    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
-    [alert addAction:cancel];
-    
-    alert.popoverPresentationController.sourceView = self.view;
-    alert.popoverPresentationController.sourceRect = [self.tableView rectForRowAtIndexPath:indexPath];
-    
-    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
