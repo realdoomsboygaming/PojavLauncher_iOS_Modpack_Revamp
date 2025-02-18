f656d5c921d2247f76ddf6096959560f399cbc04
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-17 20:41:40 -0600

    Update ModpackInstallViewController.m

--- Natives/installer/ModpackInstallViewController.m
+++ Natives/installer/ModpackInstallViewController.m
@@ -11,14 +11,17 @@
 #import "utils.h"
 #include <dlfcn.h>
 
-@interface ModpackInstallViewController () <UIContextMenuInteractionDelegate, UISearchResultsUpdating, UITableViewDataSource, UITableViewDelegate>
+#define kCurseForgeGameIDMinecraft 432
+#define kCurseForgeClassIDModpack 4471
+#define kCurseForgeClassIDMod 6
+
+@interface ModpackInstallViewController()<UIContextMenuInteractionDelegate>
 @property(nonatomic) UISearchController *searchController;
-@property(nonatomic) UISegmentedControl *sourceSegmentedControl;
 @property(nonatomic) UIMenu *currentMenu;
 @property(nonatomic) NSMutableArray *list;
 @property(nonatomic) NSMutableDictionary *filters;
-@property (nonatomic, strong) ModrinthAPI *modrinth;
-@property (nonatomic, strong) CurseForgeAPI *curseForge;
+@property ModrinthAPI *modrinth;
+@property CurseForgeAPI *curseForge;
 @end
 
 @implementation ModpackInstallViewController
@@ -26,61 +29,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Setup search controller
     self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
     self.searchController.searchResultsUpdater = self;
     self.searchController.obscuresBackgroundDuringPresentation = NO;
     self.navigationItem.searchController = self.searchController;
     
-    // Setup source selector
-    self.sourceSegmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"Modrinth", @"CurseForge"]];
-    self.sourceSegmentedControl.selectedSegmentIndex = 0;
-    [self.sourceSegmentedControl addTarget:self action:@selector(sourceChanged:) forControlEvents:UIControlEventValueChanged];
-    self.tableView.tableHeaderView = self.sourceSegmentedControl;
-    
-    // Initialize APIs
     self.modrinth = [ModrinthAPI new];
     self.curseForge = [[CurseForgeAPI alloc] init];
     
-    // Setup initial filters
-    self.filters = [@{
+    self.filters = @{
         @"isModpack": @(YES),
         @"name": @" "
-    } mutableCopy];
-    
+        // mcVersion
+    }.mutableCopy;
     [self updateSearchResults];
 }
-
-#pragma mark - Search / Filtering
 
 - (void)loadSearchResultsWithPrevList:(BOOL)prevList {
     NSString *name = self.searchController.searchBar.text;
     if (!prevList && [self.filters[@"name"] isEqualToString:name]) {
         return;
     }
-    [self switchToLoadingState];
-    self.filters[@"name"] = name;
     
-    if (self.sourceSegmentedControl.selectedSegmentIndex == 0) {
-        self.list = [self.modrinth searchModWithFilters:self.filters previousPageResult:prevList ? self.list : nil];
+    [self switchToLoadingState];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        self.filters[@"name"] = name;
+        if (self.sourceSegmentedControl.selectedSegmentIndex == 0) {
+            self.list = [self.modrinth searchModWithFilters:self.filters previousPageResult:prevList ? self.list : nil];
+        } else {
+            [self.curseForge searchModsWithFilters:self.filters previousPageResult:prevList ? self.list : nil completion:^(NSArray *results, BOOL hasMore, NSError *error) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (results) {
+                        self.list = [NSMutableArray arrayWithArray:results];
+                        [self switchToReadyState];
+                        [self.tableView reloadData];
+                    } else {
+                        showDialog(localize(@"Error", nil), error.localizedDescription);
+                        [self actionClose];
+                    }
+                });
+            }];
+        }
         dispatch_async(dispatch_get_main_queue(), ^{
             [self switchToReadyState];
             [self.tableView reloadData];
         });
-    } else {
-        [self.curseForge searchModsWithFilters:self.filters previousPageResult:prevList ? self.list : nil completion:^(NSArray *results, BOOL hasMore, NSError *error) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                if (results) {
-                    self.list = [NSMutableArray arrayWithArray:results];
-                    [self switchToReadyState];
-                    [self.tableView reloadData];
-                } else {
-                    showDialog(localize(@"Error", nil), error.localizedDescription);
-                    [self actionClose];
-                }
-            });
-        }];
-    }
+    });
 }
 
 - (void)updateSearchResults {
@@ -90,13 +84,6 @@
 - (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
     [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(updateSearchResults) object:nil];
     [self performSelector:@selector(updateSearchResults) withObject:nil afterDelay:0.5];
-}
-
-#pragma mark - UI Actions
-
-- (void)sourceChanged:(UISegmentedControl *)sender {
-    self.list = [NSMutableArray array];
-    [self updateSearchResults];
 }
 
 - (void)actionClose {
@@ -119,15 +106,23 @@
     self.tableView.allowsSelection = YES;
 }
 
-#pragma mark - UIContextMenu
+#pragma mark UIContextMenu
 
-- (UIContextMenuConfiguration *)contextMenuInteraction:(UIContextMenuInteraction *)interaction configurationForMenuAtLocation:(CGPoint)location {
+- (UIContextMenuConfiguration *)contextMenuInteraction:(UIContextMenuInteraction *)interaction configurationForMenuAtLocation:(CGPoint)location
+{
     return [UIContextMenuConfiguration configurationWithIdentifier:nil previewProvider:nil actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions) {
         return self.currentMenu;
     }];
 }
 
-#pragma mark - UITableViewDataSource
+- (_UIContextMenuStyle *)_contextMenuInteraction:(UIContextMenuInteraction *)interaction styleForMenuWithConfiguration:(UIContextMenuConfiguration *)configuration
+{
+    _UIContextMenuStyle *style = [_UIContextMenuStyle defaultStyle];
+    style.preferredLayout = 3; // _UIContextMenuLayoutCompactMenu
+    return style;
+}
+
+#pragma mark UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
@@ -144,40 +139,38 @@
         cell.imageView.contentMode = UIViewContentModeScaleToFill;
         cell.imageView.clipsToBounds = YES;
     }
-    
+
     NSDictionary *item = self.list[indexPath.row];
     cell.textLabel.text = item[@"title"];
     cell.detailTextLabel.text = item[@"description"];
-    
     UIImage *fallbackImage = [UIImage imageNamed:@"DefaultProfile"];
-    cell.imageView.image = fallbackImage;
-    
-    if (self.sourceSegmentedControl.selectedSegmentIndex == 0 && !self.modrinth.reachedLastPage && indexPath.row == self.list.count - 1) {
+    [cell.imageView setImageWithURL:[NSURL URLWithString:item[@"imageUrl"]] placeholderImage:fallbackImage];
+
+    if (!self.modrinth.reachedLastPage && indexPath.row == self.list.count-1) {
         [self loadSearchResultsWithPrevList:YES];
     }
-    
+
     return cell;
 }
 
-#pragma mark - Table View Details & Selection
-
 - (void)showDetails:(NSDictionary *)details atIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
-    
+
     NSMutableArray<UIAction *> *menuItems = [[NSMutableArray alloc] init];
-    [details[@"versionNames"] enumerateObjectsUsingBlock:^(NSString *name, NSUInteger i, BOOL *stop) {
+    [details[@"versionNames"] enumerateObjectsUsingBlock:
+    ^(NSString *name, NSUInteger i, BOOL *stop) {
         NSString *nameWithVersion = name;
         NSString *mcVersion = details[@"mcVersionNames"][i];
         if (![name hasSuffix:mcVersion]) {
             nameWithVersion = [NSString stringWithFormat:@"%@ - %@", name, mcVersion];
         }
-        [menuItems addObject:[UIAction actionWithTitle:nameWithVersion 
-                                               image:nil 
-                                             identifier:nil 
-                                           handler:^(UIAction *action) {
+        [menuItems addObject:[UIAction
+            actionWithTitle:nameWithVersion
+            image:nil identifier:nil
+            handler:^(UIAction *action) {
             [self actionClose];
             NSString *tmpIconPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"icon.png"];
-            [UIImagePNGRepresentation([cell.imageView.image _imageWithSize:CGSizeMake(40, 40)]) writeToFile:tmpIconPath atomically:YES];
+                [UIImagePNGRepresentation([cell.imageView.image _imageWithSize:CGSizeMake(40, 40)]) writeToFile:tmpIconPath atomically:YES];
             if (self.sourceSegmentedControl.selectedSegmentIndex == 0) {
                 [self.modrinth installModpackFromDetail:self.list[indexPath.row] atIndex:i];
             } else {
@@ -189,12 +182,10 @@
             }
         }]];
     }];
-    
+
     self.currentMenu = [UIMenu menuWithTitle:@"" children:menuItems];
     UIContextMenuInteraction *interaction = [[UIContextMenuInteraction alloc] initWithDelegate:self];
     cell.detailTextLabel.interactions = @[interaction];
-    
-    // Using private API as requested
     [interaction _presentMenuAtLocation:CGPointZero];
 }
 
@@ -206,7 +197,6 @@
     }
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
     [self switchToLoadingState];
-    
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         if (self.sourceSegmentedControl.selectedSegmentIndex == 0) {
             [self.modrinth loadDetailsOfMod:self.list[indexPath.row]];
