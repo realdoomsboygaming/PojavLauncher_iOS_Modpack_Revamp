55642ad4988d5ecabbdf500a65cfe1237adb111f
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-17 21:00:09 -0600

    Delete Natives/installer/DebugUtils.h

--- Natives/installer/DebugUtils.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface DebugUtils : NSObject
-
-+ (void)initializeDebugLogging;
-+ (void)log:(NSString *)message;
-+ (void)logWithLevel:(NSString *)level message:(NSString *)message;
-
-@end
