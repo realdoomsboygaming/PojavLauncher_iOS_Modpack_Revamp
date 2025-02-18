3d1a1ace43c32f0c732be2684db42e1b4607c089
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-17 19:14:43 -0600

    Update CurseForgeAPI.m

--- Natives/installer/modpack/CurseForgeAPI.m
+++ Natives/installer/modpack/CurseForgeAPI.m
@@ -1,205 +1,193 @@
-#import "CurseForgeAPI.h"
-#import <AFNetworking/AFNetworking.h>
+import Foundation
 
-#define CURSEFORGE_API_URL @"https://api.curseforge.com/v1"
-#define CURSEFORGE_MINECRAFT_GAME_ID 432
-#define CURSEFORGE_MODPACK_CLASS_ID 4471
-#define CURSEFORGE_MOD_CLASS_ID 6
-#define CURSEFORGE_PAGE_SIZE 50
+class CurseForgeAPI {
 
-@implementation CurseForgeFile
-@end
+    private let apiKey: String
 
-@implementation CurseForgeManifest
-@end
-
-@implementation CurseForgeAPI {
-    AFHTTPSessionManager *_manager;
-    NSString *_apiKey;
-}
-
-- (instancetype)initWithAPIKey:(NSString *)apiKey {
-    self = [super init];
-    if (self) {
-        _apiKey = apiKey;
-        _manager = [AFHTTPSessionManager manager];
-        _manager.requestSerializer = [AFJSONRequestSerializer serializer];
-        [_manager.requestSerializer setValue:_apiKey forHTTPHeaderField:@"x-api-key"];
+    init(apiKey: String) {
+        self.apiKey = apiKey
     }
-    return self;
-}
 
-- (void)searchModsWithFilters:(NSDictionary *)filters previousPageResult:(NSArray *)previousResults completion:(void (^)(NSArray *results, BOOL hasMore, NSError *error))completion {
-    NSMutableDictionary *params = [@{
-        @"gameId": @(CURSEFORGE_MINECRAFT_GAME_ID),
-        @"classId": filters[@"isModpack"] ? @(CURSEFORGE_MODPACK_CLASS_ID) : @(CURSEFORGE_MOD_CLASS_ID),
-        @"searchFilter": filters[@"name"] ?: @"",
-        @"pageSize": @(CURSEFORGE_PAGE_SIZE),
-        @"sortOrder": @"desc"
-    } mutableCopy];
-    
-    if (filters[@"mcVersion"]) {
-        params[@"gameVersion"] = filters[@"mcVersion"];
-    }
-    
-    if (previousResults) {
-        params[@"index"] = @([previousResults count]);
-    }
-    
-    [_manager GET:[CURSEFORGE_API_URL stringByAppendingPathComponent:@"/mods/search"]
-       parameters:params
-          headers:nil
-         progress:nil
-          success:^(NSURLSessionDataTask *task, id responseObject) {
-              NSArray *data = responseObject[@"data"];
-              NSMutableArray *results = [NSMutableArray array];
-              
-              for (NSDictionary *modData in data) {
-                  if (![modData[@"allowModDistribution"] boolValue] && ![modData[@"allowModDistribution"] isKindOfClass:[NSNull class]]) {
-                      continue;
-                  }
-                  
-                  NSDictionary *modItem = @{
-                      @"id": modData[@"id"] ?: @"",
-                      @"title": modData[@"name"] ?: @"",
-                      @"description": modData[@"summary"] ?: @"",
-                      @"imageUrl": modData[@"logo"][@"thumbnailUrl"] ?: @"",
-                      @"isModpack": filters[@"isModpack"]
-                  };
-                  [results addObject:modItem];
-              }
-              
-              NSDictionary *pagination = responseObject[@"pagination"];
-              BOOL hasMore = [pagination[@"totalCount"] integerValue] > [previousResults count] + results.count;
-              completion([previousResults arrayByAddingObjectsFromArray:results], hasMore, nil);
-          }
-          failure:^(NSURLSessionDataTask *task, NSError *error) {
-              completion(nil, NO, error);
-          }];
-}
-
-- (void)getModDetails:(NSString *)modId completion:(void (^)(NSDictionary *details, NSError *error))completion {
-    NSMutableArray *allFiles = [NSMutableArray array];
-    [self fetchPaginatedFiles:modId index:0 allFiles:allFiles completion:completion];
-}
-
-- (void)fetchPaginatedFiles:(NSString *)modId index:(NSInteger)index allFiles:(NSMutableArray *)allFiles completion:(void (^)(NSDictionary *, NSError *))completion {
-    NSDictionary *params = @{
-        @"index": @(index),
-        @"pageSize": @(CURSEFORGE_PAGE_SIZE)
-    };
-    
-    NSString *url = [NSString stringWithFormat:@"/mods/%@/files", modId];
-    [_manager GET:[CURSEFORGE_API_URL stringByAppendingPathComponent:url]
-       parameters:params
-          headers:nil
-         progress:nil
-          success:^(NSURLSessionDataTask *task, id responseObject) {
-              NSArray *data = responseObject[@"data"];
-              if (!data) {
-                  completion(nil, [NSError errorWithDomain:@"CurseForgeAPI" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Invalid response format"}]);
-                  return;
-              }
-              
-              for (NSDictionary *fileData in data) {
-                  if ([fileData[@"isServerPack"] boolValue]) continue;
-                  [allFiles addObject:fileData];
-              }
-              
-              if (data.count < CURSEFORGE_PAGE_SIZE) {
-                  [self processFiles:allFiles modId:modId completion:completion];
-              } else {
-                  [self fetchPaginatedFiles:modId index:index + data.count allFiles:allFiles completion:completion];
-              }
-          }
-          failure:^(NSURLSessionDataTask *task, NSError *error) {
-              completion(nil, error);
-          }];
-}
-
-- (void)processFiles:(NSArray *)files modId:(NSString *)modId completion:(void (^)(NSDictionary *details, NSError *error))completion {
-    NSMutableArray *versionNames = [NSMutableArray array];
-    NSMutableArray *mcVersions = [NSMutableArray array];
-    NSMutableArray *fileUrls = [NSMutableArray array];
-    NSMutableArray *hashes = [NSMutableArray array];
-    
-    for (NSDictionary *fileData in files) {
-        [versionNames addObject:fileData[@"displayName"] ?: @""];
-        
-        NSString *downloadUrl = fileData[@"downloadUrl"];
-        if (!downloadUrl) {
-            NSInteger fileId = [fileData[@"id"] integerValue];
-            NSString *fileName = fileData[@"fileName"] ?: @"";
-            downloadUrl = [NSString stringWithFormat:@"https://edge.forgecdn.net/files/%ld/%03ld/%@", 
-                         fileId/1000, fileId%1000, fileName];
-        }
-        [fileUrls addObject:downloadUrl];
-        
-        NSString *sha1 = @"";
-        for (NSDictionary *hashEntry in fileData[@"hashes"]) {
-            if ([hashEntry[@"algo"] integerValue] == 1) {
-                sha1 = hashEntry[@"value"] ?: @"";
-                break;
+    // Helper method to perform GET request
+    private func performGETRequest(url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(nil, error)
+                return
             }
+            completion(data, nil)
         }
-        [hashes addObject:sha1];
-        
-        NSString *mcVersion = @"";
-        for (NSString *version in fileData[@"gameVersions"]) {
-            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\d+\\.\\d+(\\.\\d+)?$" options:0 error:nil];
-            if ([regex numberOfMatchesInString:version options:0 range:NSMakeRange(0, version.length)] > 0) {
-                mcVersion = version;
-                break;
-            }
-        }
-        [mcVersions addObject:mcVersion];
+        task.resume()
     }
-    
-    NSDictionary *details = @{
-        @"versionNames": versionNames,
-        @"mcVersionNames": mcVersions,
-        @"versionUrls": fileUrls,
-        @"hashes": hashes,
-        @"versionDetailsLoaded": @YES
-    };
-    
-    completion(details, nil);
-}
 
-- (void)installModpackFromDetail:(NSDictionary *)detail atIndex:(NSInteger)index completion:(void (^)(BOOL success, NSError *error))completion {
-    NSString *downloadUrl = detail[@"versionUrls"][index];
-    NSURL *zipURL = [NSURL URLWithString:downloadUrl];
-    
-    NSURLSessionDownloadTask *task = [_manager downloadTaskWithRequest:[NSURLRequest requestWithURL:zipURL]
-        progress:nil
-        destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
-            return [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:response.suggestedFilename]];
+    // Helper method to perform POST request
+    private func performPOSTRequest(url: URL, parameters: [String: Any], completion: @escaping (Data?, Error?) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
+        } catch {
+            completion(nil, error)
+            return
         }
-        completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
-            if (error) {
-                completion(NO, error);
-                return;
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            completion(data, nil)
+        }
+        task.resume()
+    }
+
+    // Fetch mod details
+    func getModDetails(modId: Int, completion: @escaping (ModDetails?, Error?) -> Void) {
+        let urlString = "https://api.curseforge.com/v1/mods/\(modId)"
+        guard let url = URL(string: urlString) else {
+            completion(nil, NSError(domain: "Invalid URL", code: 400, userInfo: nil))
+            return
+        }
+        
+        performGETRequest(url: url) { data, error in
+            if let error = error {
+                completion(nil, error)
+                return
             }
             
-            NSError *zipError;
-            CurseForgeManifest *manifest = [self extractManifestFromZip:filePath.path error:&zipError];
-            if (!manifest) {
-                completion(NO, zipError);
-                return;
+            guard let data = data else {
+                completion(nil, NSError(domain: "No Data", code: 404, userInfo: nil))
+                return
             }
             
-            [self installDependencies:manifest completion:completion];
-        }];
-    
-    [task resume];
+            do {
+                let decoder = JSONDecoder()
+                let modDetails = try decoder.decode(ModDetails.self, from: data)
+                completion(modDetails, nil)
+            } catch {
+                completion(nil, error)
+            }
+        }
+    }
+
+    // Search for mods with filters
+    func searchModsWithFilters(filters: [String: Any], completion: @escaping ([Mod]?, Error?) -> Void) {
+        let urlString = "https://api.curseforge.com/v1/mods/search"
+        guard let url = URL(string: urlString) else {
+            completion(nil, NSError(domain: "Invalid URL", code: 400, userInfo: nil))
+            return
+        }
+        
+        performPOSTRequest(url: url, parameters: filters) { data, error in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, NSError(domain: "No Data", code: 404, userInfo: nil))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(ModSearchResponse.self, from: data)
+                completion(response.mods, nil)
+            } catch {
+                completion(nil, error)
+            }
+        }
+    }
+
+    // Install modpack
+    func installModpack(modpackId: Int, completion: @escaping (Bool, Error?) -> Void) {
+        let urlString = "https://api.curseforge.com/v1/modpacks/\(modpackId)/install"
+        guard let url = URL(string: urlString) else {
+            completion(false, NSError(domain: "Invalid URL", code: 400, userInfo: nil))
+            return
+        }
+        
+        let parameters: [String: Any] = ["modpackId": modpackId]
+        
+        performPOSTRequest(url: url, parameters: parameters) { data, error in
+            if let error = error {
+                completion(false, error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(false, NSError(domain: "No Data", code: 404, userInfo: nil))
+                return
+            }
+            
+            // Handle the response, e.g., check if the installation was successful
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(InstallResponse.self, from: data)
+                completion(response.success, nil)
+            } catch {
+                completion(false, error)
+            }
+        }
+    }
 }
 
-- (CurseForgeManifest *)extractManifestFromZip:(NSString *)zipPath error:(NSError **)error {
-    return nil;
+// Model for the mod details response
+struct ModDetails: Codable {
+    let id: Int
+    let name: String
+    let description: String
 }
 
-- (void)installDependencies:(CurseForgeManifest *)manifest completion:(void (^)(BOOL success, NSError *error))completion {
-    completion(YES, nil);
+// Model for the mod search response
+struct ModSearchResponse: Codable {
+    let mods: [Mod]
 }
 
-@end
+// Model for individual mod details in search results
+struct Mod: Codable {
+    let id: Int
+    let name: String
+    let slug: String
+    let downloads: Int
+}
+
+// Model for the install response
+struct InstallResponse: Codable {
+    let success: Bool
+}
+
+// Example usage of CurseForgeAPI to fetch mod details
+let curseForgeAPI = CurseForgeAPI(apiKey: "YOUR_API_KEY_HERE")
+
+// Fetch mod details
+curseForgeAPI.getModDetails(modId: 1234) { modDetails, error in
+    if let error = error {
+        print("Error fetching mod details: \(error)")
+    } else if let modDetails = modDetails {
+        print("Mod Name: \(modDetails.name), Description: \(modDetails.description)")
+    }
+}
+
+// Example usage of searching for mods with filters
+let filters: [String: Any] = ["category": "Adventure", "gameVersion": "1.16.5"]
+curseForgeAPI.searchModsWithFilters(filters: filters) { mods, error in
+    if let error = error {
+        print("Error searching for mods: \(error)")
+    } else if let mods = mods {
+        for mod in mods {
+            print("Mod Name: \(mod.name), Downloads: \(mod.downloads)")
+        }
+    }
+}
+
+// Example usage of installing a modpack
+curseForgeAPI.installModpack(modpackId: 5678) { success, error in
+    if let error = error {
+        print("Error installing modpack: \(error)")
+    } else if success {
+        print("Modpack installed successfully!")
+    }
+}
