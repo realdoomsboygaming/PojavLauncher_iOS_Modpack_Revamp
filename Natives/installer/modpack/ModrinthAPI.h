3bb78d7931778251972bed98cfed5174e329b081
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-19 17:44:43 -0600

    Update ModrinthAPI.h

--- Natives/installer/modpack/ModrinthAPI.h
+++ Natives/installer/modpack/ModrinthAPI.h
@@ -2,5 +2,8 @@
 #import "ModpackAPI.h"
 
 @interface ModrinthAPI : ModpackAPI
+
+/// Install a Modrinth modpack at the specified index inside the detail dictionary
 - (void)installModpackFromDetail:(NSDictionary *)detail atIndex:(NSInteger)index;
+
 @end
