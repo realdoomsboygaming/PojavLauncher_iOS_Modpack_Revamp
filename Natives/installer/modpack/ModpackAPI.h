be999eaeb6d52e0e562c327cc7bb31226c78dd60
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-19 17:46:19 -0600

    Update ModpackAPI.h

--- Natives/installer/modpack/ModpackAPI.h
+++ Natives/installer/modpack/ModpackAPI.h
@@ -5,17 +5,30 @@
 @class MinecraftResourceDownloadTask;
 
 @interface ModpackAPI : NSObject
-@property(nonatomic) NSString *baseURL;
-@property(nonatomic) NSError *lastError;
-@property(nonatomic) BOOL reachedLastPage;
 
+@property (nonatomic) NSString *baseURL;
+@property (nonatomic) NSError *lastError;
+@property (nonatomic) BOOL reachedLastPage;
+
+/// Initialize with a base URL string
 - (instancetype)initWithURL:(NSString *)url;
-- (NSMutableArray *)searchModWithFilters:(NSDictionary *)filters previousPageResult:(NSMutableArray *)prevResult;
+
+/// Perform a search with the given filters
+- (NSMutableArray *)searchModWithFilters:(NSDictionary *)filters
+                      previousPageResult:(NSMutableArray *)prevResult;
+
+/// Load details for a mod or modpack item in place
 - (void)loadDetailsOfMod:(NSMutableDictionary *)item;
 
+/// Install a modpack from a detail dict, picking index in the version arrays
 - (void)installModpackFromDetail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion;
-- (void)downloader:(MinecraftResourceDownloadTask *)downloader submitDownloadTasksFromPackage:(NSString *)packagePath toPath:(NSString *)destPath;
 
+/// Submits download tasks from the .mrpack or .zip package to the downloader
+- (void)downloader:(MinecraftResourceDownloadTask *)downloader
+submitDownloadTasksFromPackage:(NSString *)packagePath
+            toPath:(NSString *)destPath;
+
+/// GET request to “endpoint”
 - (id)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params;
 
 @end
