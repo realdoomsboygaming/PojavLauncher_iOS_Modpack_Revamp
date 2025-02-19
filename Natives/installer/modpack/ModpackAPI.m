43da2236cfe08ccdbde931a0c14f5c0d6d15ede1
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-19 17:46:38 -0600

    Update ModpackAPI.m

--- Natives/installer/modpack/ModpackAPI.m
+++ Natives/installer/modpack/ModpackAPI.m
@@ -5,55 +5,79 @@
 
 @implementation ModpackAPI
 
-#pragma mark Interface methods
+#pragma mark - Init
 
 - (instancetype)initWithURL:(NSString *)url {
     self = [super init];
-    self.baseURL = url;
+    if (self) {
+        self.baseURL = url;
+    }
     return self;
 }
 
+#pragma mark - Must be overridden
+
 - (void)loadDetailsOfMod:(NSMutableDictionary *)item {
+    // Subclass should override
     [self doesNotRecognizeSelector:_cmd];
 }
 
-- (NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, NSString *> *)searchFilters previousPageResult:(NSMutableArray *)prevResult {
+- (NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, NSString *> *)searchFilters
+                      previousPageResult:(NSMutableArray *)prevResult {
+    // Subclass should override
     [self doesNotRecognizeSelector:_cmd];
     return nil;
 }
 
-- (void)downloader:(MinecraftResourceDownloadTask *)downloader submitDownloadTasksFromPackage:(NSString *)packagePath toPath:(NSString *)destPath {
+- (void)downloader:(MinecraftResourceDownloadTask *)downloader
+submitDownloadTasksFromPackage:(NSString *)packagePath
+            toPath:(NSString *)destPath
+{
+    // Subclass should override
     [self doesNotRecognizeSelector:_cmd];
 }
 
+#pragma mark - Generic Endpoint
+
 - (id)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params {
-    __block id result;
+    __block id result = nil;
     dispatch_group_t group = dispatch_group_create();
     dispatch_group_enter(group);
+    
     NSString *url = [self.baseURL stringByAppendingPathComponent:endpoint];
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    [manager GET:url parameters:params headers:nil progress:nil
-    success:^(NSURLSessionTask *task, id obj) {
-        result = obj;
-        dispatch_group_leave(group);
-    } failure:^(NSURLSessionTask *operation, NSError *error) {
-        self.lastError = error;
-        dispatch_group_leave(group);
-    }];
+    
+    [manager GET:url
+      parameters:params
+        headers:nil
+       progress:nil
+        success:^(NSURLSessionTask *task, id obj) {
+            result = obj;
+            dispatch_group_leave(group);
+        }
+        failure:^(NSURLSessionTask *operation, NSError *error) {
+            self.lastError = error;
+            dispatch_group_leave(group);
+        }];
+    
     dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
-    //NSLog(@"%@", result);
     return result;
 }
 
-- (void)installModpackFromDetail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion {
-    // Pass details to LauncherNavigationController
-    NSDictionary* userInfo = @{
+#pragma mark - Install
+
+- (void)installModpackFromDetail:(NSDictionary *)modDetail
+                         atIndex:(NSUInteger)selectedVersion
+{
+    // Post a notification that something else might pick up to do the actual install
+    NSDictionary *userInfo = @{
         @"detail": modDetail,
         @"index": @(selectedVersion)
     };
-    [NSNotificationCenter.defaultCenter 
-        postNotificationName:@"InstallModpack" 
-        object:self userInfo:userInfo];
+    [NSNotificationCenter.defaultCenter
+         postNotificationName:@"InstallModpack"
+                       object:self
+                     userInfo:userInfo];
 }
 
 @end
