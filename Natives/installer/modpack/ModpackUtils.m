9cc987c30d2f82db1e2f4177146603c623ed658b
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-19 17:47:12 -0600

    Update ModpackUtils.m

--- Natives/installer/modpack/ModpackUtils.m
+++ Natives/installer/modpack/ModpackUtils.m
@@ -3,45 +3,75 @@
 
 @implementation ModpackUtils
 
-+ (void)archive:(UZKArchive *)archive extractDirectory:(NSString *)dir toPath:(NSString *)path error:(NSError *__autoreleasing*)error {
++ (void)archive:(UZKArchive *)archive
+extractDirectory:(NSString *)dir
+         toPath:(NSString *)path
+          error:(NSError *__autoreleasing*)error
+{
     [archive performOnFilesInArchive:^(UZKFileInfo *fileInfo, BOOL *stop) {
-        if (![fileInfo.filename hasPrefix:dir] ||
-            fileInfo.filename.length <= dir.length) {
+        // Only extract if file is in the requested subdirectory
+        if (![fileInfo.filename hasPrefix:dir] || fileInfo.filename.length <= dir.length) {
             return;
         }
-        NSString *fileName = [fileInfo.filename substringFromIndex:dir.length+1];
+        
+        // Destination path
+        NSString *fileName = [fileInfo.filename substringFromIndex:dir.length + 1];
         NSString *destItemPath = [path stringByAppendingPathComponent:fileName];
-        NSString *destDirPath = fileInfo.isDirectory ? destItemPath : destItemPath.stringByDeletingLastPathComponent;
-        BOOL createdDir = [NSFileManager.defaultManager createDirectoryAtPath:destDirPath
-            withIntermediateDirectories:YES
-            attributes:nil error:error];
+        
+        // Create directory if needed
+        NSString *destDirPath = fileInfo.isDirectory
+                                ? destItemPath
+                                : destItemPath.stringByDeletingLastPathComponent;
+        BOOL createdDir = [NSFileManager.defaultManager
+            createDirectoryAtPath:destDirPath
+      withIntermediateDirectories:YES
+                       attributes:nil
+                            error:error];
         if (!createdDir) {
             *stop = YES;
             return;
         } else if (fileInfo.isDirectory) {
             return;
         }
-
+        
+        // Extract data
         NSData *data = [archive extractData:fileInfo error:error];
-        BOOL written = [data writeToFile:destItemPath options:NSDataWritingAtomic error:error];
+        if (!data || *error) {
+            *stop = YES;
+            return;
+        }
+        
+        BOOL written = [data writeToFile:destItemPath
+                                 options:NSDataWritingAtomic
+                                   error:error];
         *stop = !data || !written;
         if (!*stop) {
-            NSLog(@"[ModpackDL] Extracted %@", fileInfo.filename);
+            NSLog(@"[ModpackUtils] Extracted %@", fileInfo.filename);
         }
     } error:error];
 }
 
 + (NSDictionary *)infoForDependencies:(NSDictionary *)dependency {
+    if (![dependency isKindOfClass:[NSDictionary class]]) {
+        return @{};
+    }
+    
     NSMutableDictionary *info = [NSMutableDictionary new];
     NSString *minecraftVersion = dependency[@"minecraft"];
+    if (!minecraftVersion) {
+        return @{};
+    }
+    
     if (dependency[@"forge"]) {
         info[@"id"] = [NSString stringWithFormat:@"%@-forge-%@", minecraftVersion, dependency[@"forge"]];
     } else if (dependency[@"fabric-loader"]) {
         info[@"id"] = [NSString stringWithFormat:@"fabric-loader-%@-%@", dependency[@"fabric-loader"], minecraftVersion];
-        info[@"json"] = [NSString stringWithFormat:FabricUtils.endpoints[@"Fabric"][@"json"], minecraftVersion, dependency[@"fabric-loader"]];
+        info[@"json"] = [NSString stringWithFormat:FabricUtils.endpoints[@"Fabric"][@"json"],
+                         minecraftVersion, dependency[@"fabric-loader"]];
     } else if (dependency[@"quilt-loader"]) {
         info[@"id"] = [NSString stringWithFormat:@"quilt-loader-%@-%@", dependency[@"quilt-loader"], minecraftVersion];
-        info[@"json"] = [NSString stringWithFormat:FabricUtils.endpoints[@"Quilt"][@"json"], minecraftVersion, dependency[@"quilt-loader"]];
+        info[@"json"] = [NSString stringWithFormat:FabricUtils.endpoints[@"Quilt"][@"json"],
+                         minecraftVersion, dependency[@"quilt-loader"]];
     }
     return info;
 }
