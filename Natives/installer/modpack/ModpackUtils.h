85ae0f23c425c7cc9c5ccd8d65162658d426b5cd
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-19 17:46:53 -0600

    Update ModpackUtils.h

--- Natives/installer/modpack/ModpackUtils.h
+++ Natives/installer/modpack/ModpackUtils.h
@@ -3,7 +3,13 @@
 
 @interface ModpackUtils : NSObject
 
-+ (void)archive:(UZKArchive *)archive extractDirectory:(NSString *)dir toPath:(NSString *)path error:(NSError **)error;
+/// Extract a directory named `dir` from the UZKArchive into the file system
++ (void)archive:(UZKArchive *)archive
+extractDirectory:(NSString *)dir
+         toPath:(NSString *)path
+          error:(NSError **)error;
+
+/// Produce a dictionary with "id" (and optional "json") for the given dependencies
 + (NSDictionary *)infoForDependencies:(NSDictionary *)dependency;
 
 @end
