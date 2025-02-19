1383e746a679642052921f25fdd7df90933da8a7
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-19 17:47:56 -0600

    Update ModpackInstallViewController.m

--- Natives/installer/ModpackInstallViewController.m
+++ Natives/installer/ModpackInstallViewController.m
@@ -5,17 +5,12 @@
 #import "utils.h"
 #import "config.h"
 
-// Note: Because this file is in Natives/installer and the API files are in the modpack subfolder,
-// import using the relative path. Alternatively, update your Header Search Paths.
+// The imports below might be redundant given the .h includes, but this is okay
 #import "modpack/CurseForgeAPI.h"
 #import "modpack/ModrinthAPI.h"
 
-@interface ModpackInstallViewController () <UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating, UIContextMenuInteractionDelegate>
-
-// We hold a strong reference to the table view as a separate property
-@property (nonatomic, strong) UITableView *tableView;
+@interface ModpackInstallViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UIImage *fallbackImage;
-
 @end
 
 @implementation ModpackInstallViewController
@@ -23,30 +18,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Initialize TableView as a separate object, then add it to self.view
-    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    // We already inherit from UITableViewController,
+    // so self.tableView is the main view. By default,
+    // UITableViewController sets self.view = self.tableView in its init.
+    // (This is how UITableViewController is designed.)
+    //
+    // => If you add an additional table view, or do "self.view = [UIView new]"
+    //    inside a UITableViewController, you'd risk confusion or subview errors.
+    //
+    // So simply ensure we do NOT reassign self.view or do a second "addSubview:"
+    // for that same table. We'll just configure the existing self.tableView.
+
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
-    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [self.view addSubview:self.tableView];
-    
-    // Configure Search Controller
+
+    // Create and configure a search controller
     self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
-    self.searchController.searchResultsUpdater = self;
+    self.searchController.searchResultsUpdater = self; // We'll handle updates
     self.searchController.obscuresBackgroundDuringPresentation = NO;
     
-    // If you support iOS 11+ and above, you can attach it to navigationItem like this:
-    // For iOS 14 compatibility, this is fine since iOS 14 >= 11.
+    // For iOS >= 11, this is valid:
     self.navigationItem.searchController = self.searchController;
-
-    // Initialize APIs
+    
+    // Initialize our API objects
     self.modrinth = [ModrinthAPI new];
     NSString *key = [[NSUserDefaults standardUserDefaults] stringForKey:@"CURSEFORGE_API_KEY"];
     if (key.length > 0) {
         self.curseForge = [[CurseForgeAPI alloc] initWithAPIKey:key];
     }
     
-    // Configure Segmented Control
+    // Segmented control for switching between CF and Modrinth
     self.apiSegmentControl = [[UISegmentedControl alloc] initWithItems:@[@"CurseForge", @"Modrinth"]];
     self.apiSegmentControl.selectedSegmentIndex = 0;
     self.apiSegmentControl.frame = CGRectMake(0, 0, 200, 30);
@@ -55,15 +56,15 @@
                      forControlEvents:UIControlEventValueChanged];
     self.navigationItem.titleView = self.apiSegmentControl;
     
-    // Initialize Filters
+    // Set up filters
     self.filters = [@{@"isModpack": @(YES), @"name": @""} mutableCopy];
     self.fallbackImage = [UIImage imageNamed:@"DefaultProfile"];
     
-    // First load
+    // Trigger initial search
     [self updateSearchResults];
 }
 
-#pragma mark - Segment Control Handler
+#pragma mark - UISegmentedControl Action
 
 - (void)apiSegmentChanged:(UISegmentedControl *)sender {
     [self.list removeAllObjects];
@@ -71,40 +72,7 @@
     [self updateSearchResults];
 }
 
-#pragma mark - Context Menu Delegate
-
-- (UIContextMenuConfiguration *)contextMenuInteraction:(UIContextMenuInteraction *)interaction
-                         configurationForMenuAtLocation:(CGPoint)location
-{
-    // Return a configuration that yields self.currentMenu
-    return [UIContextMenuConfiguration
-        configurationWithIdentifier:nil
-        previewProvider:nil
-        actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions) {
-            return self.currentMenu;
-        }];
-}
-
-#pragma mark - TableView Delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    [self showDetails:self.list[indexPath.row] atIndexPath:indexPath];
-}
-
-- (UIContextMenuConfiguration *)tableView:(UITableView *)tableView
-          contextMenuConfigurationForRowAtIndexPath:(NSIndexPath *)indexPath
-                                              point:(CGPoint)point
-{
-    return [UIContextMenuConfiguration
-        configurationWithIdentifier:nil
-        previewProvider:nil
-        actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions) {
-            return self.currentMenu;
-        }];
-}
-
-#pragma mark - Search Handling
+#pragma mark - UISearchResultsUpdating
 
 - (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
     [NSObject cancelPreviousPerformRequestsWithTarget:self
@@ -119,11 +87,15 @@
     [self loadSearchResultsWithPrevList:NO];
 }
 
+#pragma mark - Loading Search Results
+
 - (void)loadSearchResultsWithPrevList:(BOOL)prevList {
-    NSString *name = self.searchController.searchBar.text;
-    // Ensure filters[@"name"] is a valid string (default to empty string if not)
-    NSString *previousName = ([self.filters[@"name"] isKindOfClass:[NSString class]] ? self.filters[@"name"] : @"");
-    if (!prevList && [previousName isEqualToString:name]) return;
+    NSString *name = self.searchController.searchBar.text ?: @"";
+    NSString *previousName = ([self.filters[@"name"] isKindOfClass:[NSString class]] ?
+                              self.filters[@"name"] : @"");
+    if (!prevList && [previousName isEqualToString:name]) {
+        return;
+    }
     
     [self switchToLoadingState];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
@@ -132,14 +104,14 @@
         NSMutableArray *results = nil;
         
         if (self.apiSegmentControl.selectedSegmentIndex == 0) {
-            // Searching with CurseForge
+            // CurseForge
             results = [self.curseForge searchModWithFilters:self.filters
-                                         previousPageResult:prevList ? self.list : nil];
+                                         previousPageResult:(prevList ? self.list : nil)];
             searchError = self.curseForge.lastError;
         } else {
-            // Searching with Modrinth
+            // Modrinth
             results = [self.modrinth searchModWithFilters:self.filters
-                                       previousPageResult:prevList ? self.list : nil];
+                                       previousPageResult:(prevList ? self.list : nil)];
             searchError = self.modrinth.lastError;
         }
         
@@ -147,7 +119,7 @@
             if (results) {
                 self.list = results;
                 [self.tableView reloadData];
-            } else {
+            } else if (searchError) {
                 showDialog(localize(@"Error", nil), searchError.localizedDescription);
             }
             [self switchToReadyState];
@@ -170,16 +142,16 @@
         cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
         cell.imageView.clipsToBounds = YES;
         
-        // Add context menu interaction to new cells (iOS 13+, so iOS 14 is fine)
+        // Add context menu interaction (iOS 13+)
         UIContextMenuInteraction *interaction = [[UIContextMenuInteraction alloc] initWithDelegate:self];
         [cell addInteraction:interaction];
     }
     
     NSDictionary *item = self.list[indexPath.row];
-    cell.textLabel.text = ([item[@"title"] isKindOfClass:[NSString class]] ? item[@"title"] : @"Untitled");
+    cell.textLabel.text       = ([item[@"title"] isKindOfClass:[NSString class]] ? item[@"title"] : @"Untitled");
     cell.detailTextLabel.text = ([item[@"description"] isKindOfClass:[NSString class]] ? item[@"description"] : @"No description");
     
-    // Image Loading
+    // Load image
     NSString *imageUrl = ([item[@"imageUrl"] isKindOfClass:[NSString class]] ? item[@"imageUrl"] : @"");
     if (imageUrl.length > 0) {
         [self loadImageForCell:cell withURL:imageUrl];
@@ -187,9 +159,10 @@
         cell.imageView.image = self.fallbackImage;
     }
     
-    // Pagination
+    // Pagination support
     BOOL usingCurseForge = (self.apiSegmentControl.selectedSegmentIndex == 0);
-    BOOL reachedLastPage = usingCurseForge ? self.curseForge.reachedLastPage : self.modrinth.reachedLastPage;
+    BOOL reachedLastPage = usingCurseForge ? self.curseForge.reachedLastPage
+                                           : self.modrinth.reachedLastPage;
     if (!reachedLastPage && indexPath.row == self.list.count - 1) {
         [self loadSearchResultsWithPrevList:YES];
     }
@@ -197,7 +170,41 @@
     return cell;
 }
 
-#pragma mark - Context Menu Handling
+#pragma mark - TableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self showDetails:self.list[indexPath.row] atIndexPath:indexPath];
+}
+
+#pragma mark - Context Menu (iOS 13+)
+
+- (UIContextMenuConfiguration *)tableView:(UITableView *)tableView
+   contextMenuConfigurationForRowAtIndexPath:(NSIndexPath *)indexPath
+                                       point:(CGPoint)point
+{
+    return [UIContextMenuConfiguration
+        configurationWithIdentifier:nil
+        previewProvider:nil
+        actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions)
+    {
+        return self.currentMenu; // Provide custom menu or dynamically build
+    }];
+}
+
+- (UIContextMenuConfiguration *)contextMenuInteraction:(UIContextMenuInteraction *)interaction
+                         configurationForMenuAtLocation:(CGPoint)location
+{
+    // Return a configuration that yields self.currentMenu
+    return [UIContextMenuConfiguration configurationWithIdentifier:nil
+                                                     previewProvider:nil
+                                                      actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions)
+    {
+        return self.currentMenu;
+    }];
+}
+
+#pragma mark - Show Details
 
 - (void)showDetails:(NSDictionary *)details atIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
@@ -210,12 +217,13 @@
     NSArray *mcVersionNames = details[@"mcVersionNames"];
     
     if (![versionNames isKindOfClass:[NSArray class]] ||
-        ![mcVersionNames isKindOfClass:[NSArray class]]) {
+        ![mcVersionNames isKindOfClass:[NSArray class]])
+    {
         return;
     }
     
     [versionNames enumerateObjectsUsingBlock:^(NSString *name, NSUInteger i, BOOL *stop) {
-        NSString *mcVersion = (mcVersionNames.count > i ? mcVersionNames[i] : @"");
+        NSString *mcVersion = (i < mcVersionNames.count ? mcVersionNames[i] : @"");
         NSString *nameWithVersion = ([name containsString:mcVersion]
                                      ? name
                                      : [NSString stringWithFormat:@"%@ - %@", name, mcVersion]);
@@ -224,22 +232,23 @@
                                                                 style:UIAlertActionStyleDefault
                                                               handler:^(UIAlertAction * _Nonnull action)
         {
-            // Save the image from the cell.
+            // Save the cell image to a temp location
             NSString *tmpIconPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"icon.png"];
             NSData *imageData = UIImagePNGRepresentation(cell.imageView.image);
             [imageData writeToFile:tmpIconPath atomically:YES];
             
-            // Install modpack using the appropriate API
+            // Install
             if (self.apiSegmentControl.selectedSegmentIndex == 0) {
+                // CurseForge
                 [self.curseForge installModpackFromDetail:self.list[indexPath.row]
                                                    atIndex:i];
             } else {
+                // Modrinth
                 [self.modrinth installModpackFromDetail:self.list[indexPath.row]
                                                  atIndex:i];
             }
             [self actionClose];
         }];
-        
         [alert addAction:versionAction];
     }];
     
@@ -247,7 +256,7 @@
                                               style:UIAlertActionStyleCancel
                                             handler:nil]];
     
-    // Required on iPad; also helpful on iPhone if using a popover style
+    // On iPad, need a popover anchor
     if (alert.popoverPresentationController) {
         alert.popoverPresentationController.sourceView = cell;
         alert.popoverPresentationController.sourceRect = cell.bounds;
@@ -265,20 +274,21 @@
     [indicator startAnimating];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:indicator];
     
-    // modalInPresentation is iOS 13+, so iOS 14 is definitely okay
-    self.navigationController.modalInPresentation = YES;
+    // Lock the UI so user can't close
+    self.navigationController.modalInPresentation = YES; // iOS 13+
     self.tableView.allowsSelection = NO;
 }
 
 - (void)switchToReadyState {
-    // Restore the bar button item to a close button (iOS 13+ for the .Close system item)
     UIActivityIndicatorView *indicator = (id)self.navigationItem.rightBarButtonItem.customView;
     [indicator stopAnimating];
     
-    self.navigationItem.rightBarButtonItem =
-      [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemClose
-                                                    target:self
-                                                    action:@selector(actionClose)];
+    // iOS 13+ has UIBarButtonSystemItemClose, safe for iOS 14
+    UIBarButtonItem *closeButton = [[UIBarButtonItem alloc]
+                                    initWithBarButtonSystemItem:UIBarButtonSystemItemClose
+                                                         target:self
+                                                         action:@selector(actionClose)];
+    self.navigationItem.rightBarButtonItem = closeButton;
     
     self.navigationController.modalInPresentation = NO;
     self.tableView.allowsSelection = YES;
@@ -301,9 +311,9 @@
                                                       NSError *error)
     {
         if (!error && data) {
-            UIImage *downloadedImage = [UIImage imageWithData:data];
+            UIImage *img = [UIImage imageWithData:data];
             dispatch_async(dispatch_get_main_queue(), ^{
-                cell.imageView.image = downloadedImage ?: self.fallbackImage;
+                cell.imageView.image = img ?: self.fallbackImage;
                 [cell setNeedsLayout];
             });
         }
