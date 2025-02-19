c0e760bb40b8c2a97ee8eab00932abdfa3b2903b
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-19 11:30:23 -0600

    Update ModrinthAPI.h

--- Natives/installer/modpack/ModrinthAPI.h
+++ Natives/installer/modpack/ModrinthAPI.h
@@ -1,5 +1,14 @@
 #import <Foundation/Foundation.h>
-#import "ModpackAPI.h"
+#import "MinecraftResourceDownloadTask.h"
 
-@interface ModrinthAPI : ModpackAPI
+@interface ModrinthAPI : NSObject
+
+@property (nonatomic) BOOL reachedLastPage;
+@property (nonatomic, strong) NSString *lastSearchTerm;
+
+- (instancetype)init;
+- (NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, NSString *> *)searchFilters previousPageResult:(NSMutableArray *)modrinthSearchResult;
+- (void)loadDetailsOfMod:(NSMutableDictionary *)item;
+- (void)downloader:(MinecraftResourceDownloadTask *)downloader submitDownloadTasksFromPackage:(NSString *)packagePath toPath:(NSString *)destPath;
+
 @end
