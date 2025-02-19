71967b7fde4487eed1c24f4370f2e833db733b23
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-18 18:50:33 -0600

    Update CurseForgeAPI.h

--- Natives/installer/modpack/CurseForgeAPI.h
+++ Natives/installer/modpack/CurseForgeAPI.h
@@ -1,11 +1,5 @@
-// Natives/modpack/CurseForgeAPI.h
 #import <Foundation/Foundation.h>
+#import "ModpackAPI.h"
 
-@interface CurseForgeAPI : NSObject
-
-- (instancetype)initWithAPIKey:(NSString *)apiKey;
-- (void)searchModsWithFilters:(NSDictionary *)filters previousPageResult:(NSArray *)previousResults completion:(void (^)(NSArray *results, BOOL hasMore, NSError *error))completion;
-- (void)getModDetails:(NSString *)modId completion:(void (^)(NSDictionary *details, NSError *error))completion;
-- (void)installModpackFromDetail:(NSDictionary *)detail atIndex:(NSInteger)index completion:(void (^)(BOOL success, NSError *error))completion;
-
+@interface CurseForgeAPI : ModpackAPI
 @end
