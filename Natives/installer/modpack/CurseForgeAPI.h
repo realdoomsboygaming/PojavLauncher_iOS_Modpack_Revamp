a403e17807a5ea9d5940ed4fe043a31869cd7cb6
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-19 16:15:58 -0600

    Update CurseForgeAPI.h

--- Natives/installer/modpack/CurseForgeAPI.h
+++ Natives/installer/modpack/CurseForgeAPI.h
@@ -1,15 +1,23 @@
+#ifndef CurseForgeAPI_h
+#define CurseForgeAPI_h
+
 #import <Foundation/Foundation.h>
-#import "MinecraftResourceDownloadTask.h"
 
 @interface CurseForgeAPI : NSObject
-@property (nonatomic) BOOL reachedLastPage;
-@property (nonatomic) NSInteger previousOffset;
-@property (nonatomic, strong) NSString *lastSearchTerm;
-@property (nonatomic, strong) NSString *apiKey;
+
+@property (nonatomic, strong, readonly) NSString *apiKey;
 @property (nonatomic, strong) NSError *lastError;
+@property (nonatomic, assign) NSInteger previousOffset;
+@property (nonatomic, assign) BOOL reachedLastPage;
+@property (nonatomic, strong) NSString *lastSearchTerm;
 
 - (instancetype)initWithAPIKey:(NSString *)apiKey;
+- (NSString *)loadAPIKey;
+- (id)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params;
 - (NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, id> *)searchFilters previousPageResult:(NSMutableArray *)previousResults;
 - (void)installModpackFromDetail:(NSDictionary *)detail atIndex:(NSInteger)index;
 - (void)loadDetailsOfMod:(NSMutableDictionary *)item;
+
 @end
+
+#endif /* CurseForgeAPI_h */
