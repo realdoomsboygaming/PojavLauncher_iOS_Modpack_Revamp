46684f4f243c6a93e53282276a382287af1073ff
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-20 11:35:13 -0600

    Update CurseForgeAPI.h

--- Natives/installer/modpack/CurseForgeAPI.h
+++ Natives/installer/modpack/CurseForgeAPI.h
@@ -2,4 +2,18 @@
 #import "ModpackAPI.h"
 
 @interface CurseForgeAPI : ModpackAPI
+
+- (instancetype)initWithAPIKey:(NSString *)apiKey;
+- (void)searchModWithFilters:(NSDictionary *)searchFilters
+         previousPageResult:(NSMutableArray *)prevResult
+                 completion:(void (^)(NSMutableArray * _Nullable results, NSError * _Nullable error))completion;
+- (void)loadDetailsOfMod:(NSMutableDictionary *)item
+              completion:(void (^)(NSError * _Nullable error))completion;
+- (void)installModpackFromDetail:(NSDictionary *)modDetail
+                         atIndex:(NSUInteger)selectedVersion
+                      completion:(void (^)(NSError * _Nullable error))completion;
+
+/// The parent view controller is used for presenting dialogs (if needed)
+@property (nonatomic, weak) UIViewController *parentViewController;
+
 @end
