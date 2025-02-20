9209e967d666c1ca5b9ae763ac57814c956087f4
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-20 08:39:53 -0600

    Update ModpackInstallViewController.m

--- Natives/installer/ModpackInstallViewController.m
+++ Natives/installer/ModpackInstallViewController.m
@@ -21,7 +21,7 @@
     // Setup table view (since we’re a UITableViewController)
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
-
+    
     // Create and configure a search controller
     self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
     self.searchController.searchResultsUpdater = self;
@@ -43,6 +43,8 @@
     NSString *storedKey = [[NSUserDefaults standardUserDefaults] stringForKey:@"CURSEFORGE_API_KEY"];
     if (storedKey.length > 0) {
         self.curseForge = [[CurseForgeAPI alloc] initWithAPIKey:storedKey];
+        // Set the parent view controller for integrated browser fallback
+        self.curseForge.parentViewController = self;
     } else {
         // Prompt the user for a key right away if we’re on the CF tab
         if (self.apiSegmentControl.selectedSegmentIndex == 0) {
@@ -67,7 +69,6 @@
 #pragma mark - Prompt for CF key if missing
 
 - (void)promptForCurseForgeAPIKey {
-    // iOS 8+ (including 14) supports UIAlertController with text fields
     UIAlertController *alert =
       [UIAlertController alertControllerWithTitle:@"CurseForge API Key"
                                           message:@"Please enter your CurseForge API key"
@@ -87,9 +88,9 @@
             [[NSUserDefaults standardUserDefaults] setObject:key forKey:@"CURSEFORGE_API_KEY"];
             [[NSUserDefaults standardUserDefaults] synchronize];
             
-            // Instantiate the CF object
+            // Instantiate the CF object and assign parent view controller for fallback
             self.curseForge = [[CurseForgeAPI alloc] initWithAPIKey:key];
-            // Re-run the search in case user typed something
+            self.curseForge.parentViewController = self;
             [self updateSearchResults];
         } else {
             // If user provided no key, default to Modrinth
@@ -103,7 +104,7 @@
                                                            style:UIAlertActionStyleCancel
                                                          handler:^(UIAlertAction * _Nonnull action)
     {
-        // If the user cancels, we’ll just switch to Modrinth automatically
+        // If the user cancels, switch to Modrinth automatically
         self.apiSegmentControl.selectedSegmentIndex = 1;
         [self updateSearchResults];
     }];
@@ -148,15 +149,13 @@
 
 - (void)loadSearchResultsWithPrevList:(BOOL)prevList {
     NSString *name = self.searchController.searchBar.text ?: @"";
-    NSString *previousName = ([self.filters[@"name"] isKindOfClass:[NSString class]]
-                              ? self.filters[@"name"] : @"");
+    NSString *previousName = ([self.filters[@"name"] isKindOfClass:[NSString class]] ? self.filters[@"name"] : @"");
     if (!prevList && [previousName isEqualToString:name]) {
         return;
     }
     
     [self switchToLoadingState];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        
         self.filters[@"name"] = name;
         NSError *searchError = nil;
         NSMutableArray *results = nil;
@@ -200,12 +199,10 @@
     return self.list.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
-                                      reuseIdentifier:@"cell"];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"];
         cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
         cell.imageView.clipsToBounds = YES;
         
@@ -215,7 +212,7 @@
     }
     
     NSDictionary *item = self.list[indexPath.row];
-    cell.textLabel.text       = ([item[@"title"] isKindOfClass:[NSString class]] ? item[@"title"] : @"Untitled");
+    cell.textLabel.text = ([item[@"title"] isKindOfClass:[NSString class]] ? item[@"title"] : @"Untitled");
     cell.detailTextLabel.text = ([item[@"description"] isKindOfClass:[NSString class]] ? item[@"description"] : @"No description");
     
     // Load image
@@ -228,8 +225,7 @@
     
     // Handle “infinite scrolling” if the API supports pages
     BOOL usingCurseForge = (self.apiSegmentControl.selectedSegmentIndex == 0);
-    BOOL reachedLastPage = usingCurseForge ? self.curseForge.reachedLastPage
-                                           : self.modrinth.reachedLastPage;
+    BOOL reachedLastPage = usingCurseForge ? self.curseForge.reachedLastPage : self.modrinth.reachedLastPage;
     if (!reachedLastPage && indexPath.row == self.list.count - 1) {
         [self loadSearchResultsWithPrevList:YES];
     }
@@ -239,7 +235,6 @@
 
 #pragma mark - TableView Delegate
 
-// Modified didSelectRowAtIndexPath to ensure version details are loaded before showing version selection
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
@@ -254,7 +249,6 @@
             } else {
                 [self.modrinth loadDetailsOfMod:item];
             }
-            // Mark details as loaded
             item[@"versionDetailsLoaded"] = @(YES);
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self showDetails:item atIndexPath:indexPath];
@@ -269,23 +263,19 @@
 
 - (UIContextMenuConfiguration *)tableView:(UITableView *)tableView
    contextMenuConfigurationForRowAtIndexPath:(NSIndexPath *)indexPath
-                                       point:(CGPoint)point
-{
+                                       point:(CGPoint)point {
     return [UIContextMenuConfiguration configurationWithIdentifier:nil
                                                      previewProvider:nil
-                                                      actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions)
-    {
+                                                      actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions) {
         return self.currentMenu;
     }];
 }
 
 - (UIContextMenuConfiguration *)contextMenuInteraction:(UIContextMenuInteraction *)interaction
-                         configurationForMenuAtLocation:(CGPoint)location
-{
+                         configurationForMenuAtLocation:(CGPoint)location {
     return [UIContextMenuConfiguration configurationWithIdentifier:nil
                                                      previewProvider:nil
-                                                      actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions)
-    {
+                                                      actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions) {
         return self.currentMenu;
     }];
 }
@@ -299,26 +289,21 @@
                                                                    message:nil
                                                             preferredStyle:UIAlertControllerStyleActionSheet];
     
-    NSArray *versionNames   = details[@"versionNames"];
+    NSArray *versionNames = details[@"versionNames"];
     NSArray *mcVersionNames = details[@"mcVersionNames"];
     
-    if (![versionNames isKindOfClass:[NSArray class]] ||
-        ![mcVersionNames isKindOfClass:[NSArray class]])
-    {
+    if (![versionNames isKindOfClass:[NSArray class]] || ![mcVersionNames isKindOfClass:[NSArray class]]) {
         return;
     }
     
     [versionNames enumerateObjectsUsingBlock:^(NSString *name, NSUInteger i, BOOL *stop) {
         NSString *mcVersion = (i < mcVersionNames.count ? mcVersionNames[i] : @"");
-        NSString *fullText = ([name rangeOfString:mcVersion].location != NSNotFound
-                            ? name
-                            : [NSString stringWithFormat:@"%@ - %@", name, mcVersion]);
+        NSString *fullText = ([name rangeOfString:mcVersion].location != NSNotFound ? name : [NSString stringWithFormat:@"%@ - %@", name, mcVersion]);
         
         UIAlertAction *versionAction = [UIAlertAction actionWithTitle:fullText
                                                                 style:UIAlertActionStyleDefault
-                                                              handler:^(UIAlertAction * _Nonnull action)
-        {
-            // Save the cell’s image to a temp location for the final profile’s icon
+                                                              handler:^(UIAlertAction * _Nonnull action) {
+            // Save the cell’s image to a temporary location for the final profile’s icon
             NSString *tmpIconPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"icon.png"];
             NSData *imgData = UIImagePNGRepresentation(cell.imageView.image);
             [imgData writeToFile:tmpIconPath atomically:YES];
@@ -338,7 +323,6 @@
                                               style:UIAlertActionStyleCancel
                                             handler:nil]];
     
-    // iPad popover anchor
     if (alert.popoverPresentationController) {
         alert.popoverPresentationController.sourceView = cell;
         alert.popoverPresentationController.sourceRect = cell.bounds;
@@ -351,18 +335,15 @@
 #pragma mark - UI State Management
 
 - (void)switchToLoadingState {
-    UIActivityIndicatorView *indicator =
-      [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleMedium];
+    UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleMedium];
     [indicator startAnimating];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:indicator];
     
-    // iOS13+ property, safe for iOS14
     self.navigationController.modalInPresentation = YES;
     self.tableView.allowsSelection = NO;
 }
 
 - (void)switchToReadyState {
-    // Revert to a Close button
     UIActivityIndicatorView *indicator = (id)self.navigationItem.rightBarButtonItem.customView;
     [indicator stopAnimating];
     
@@ -388,8 +369,7 @@
     
     NSURLSessionDataTask *task = [[NSURLSession sharedSession]
       dataTaskWithURL:url
-    completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
-    {
+    completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         if (!error && data) {
             UIImage *img = [UIImage imageWithData:data];
             dispatch_async(dispatch_get_main_queue(), ^{
