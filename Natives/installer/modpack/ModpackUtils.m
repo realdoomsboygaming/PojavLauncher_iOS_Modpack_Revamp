818d535d16610fa311b7e3fd383c0568e29d1111
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-20 15:11:54 -0600

    Update ModpackUtils.m

--- Natives/installer/modpack/ModpackUtils.m
+++ Natives/installer/modpack/ModpackUtils.m
@@ -9,12 +9,13 @@
             fileInfo.filename.length <= dir.length) {
             return;
         }
-        NSString *fileName = [fileInfo.filename substringFromIndex:dir.length+1];
+        NSString *fileName = [fileInfo.filename substringFromIndex:dir.length + 1];
         NSString *destItemPath = [path stringByAppendingPathComponent:fileName];
         NSString *destDirPath = fileInfo.isDirectory ? destItemPath : destItemPath.stringByDeletingLastPathComponent;
         BOOL createdDir = [NSFileManager.defaultManager createDirectoryAtPath:destDirPath
-            withIntermediateDirectories:YES
-            attributes:nil error:error];
+                                                withIntermediateDirectories:YES
+                                                                 attributes:nil
+                                                                      error:error];
         if (!createdDir) {
             *stop = YES;
             return;
@@ -32,14 +33,21 @@
 }
 
 + (NSDictionary *)infoForDependencies:(NSDictionary *)dependency {
+    // This method checks the "dependencies" dictionary for keys like "forge", "fabric-loader", etc.
+    // and builds a dictionary with the "id" and possibly a "json" URL for the dependency.
     NSMutableDictionary *info = [NSMutableDictionary new];
     NSString *minecraftVersion = dependency[@"minecraft"];
+
     if (dependency[@"forge"]) {
+        // For example: "1.16.5-forge-36.2.0"
         info[@"id"] = [NSString stringWithFormat:@"%@-forge-%@", minecraftVersion, dependency[@"forge"]];
     } else if (dependency[@"fabric-loader"]) {
+        // e.g. "fabric-loader-0.14.17-1.19"
         info[@"id"] = [NSString stringWithFormat:@"fabric-loader-%@-%@", dependency[@"fabric-loader"], minecraftVersion];
+        // Download the version JSON from your FabricUtils endpoints
         info[@"json"] = [NSString stringWithFormat:FabricUtils.endpoints[@"Fabric"][@"json"], minecraftVersion, dependency[@"fabric-loader"]];
     } else if (dependency[@"quilt-loader"]) {
+        // e.g. "quilt-loader-0.14.17-1.19"
         info[@"id"] = [NSString stringWithFormat:@"quilt-loader-%@-%@", dependency[@"quilt-loader"], minecraftVersion];
         info[@"json"] = [NSString stringWithFormat:FabricUtils.endpoints[@"Quilt"][@"json"], minecraftVersion, dependency[@"quilt-loader"]];
     }
