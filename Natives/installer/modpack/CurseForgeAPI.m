4d833ba7df1d0bbb275f58f7c008e011eb265316
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-19 18:55:34 -0600

    Update CurseForgeAPI.m

--- Natives/installer/modpack/CurseForgeAPI.m
+++ Natives/installer/modpack/CurseForgeAPI.m
@@ -1,288 +1,153 @@
 #import "CurseForgeAPI.h"
-#import "utils.h"
 #import "UZKArchive.h"
 #import "ModpackUtils.h"
-
-static const NSInteger kCurseForgeGameIDMinecraft = 432;
-static const NSInteger kCurseForgeClassIDModpack  = 4471;
-static const NSInteger kCurseForgeClassIDMod      = 6;
+#import "PLProfiles.h"
 
 @implementation CurseForgeAPI
 
-#pragma mark - Init & API Key
+#pragma mark - Install a modpack (inline approach)
 
-- (instancetype)initWithAPIKey:(NSString *)apiKey {
-    self = [super init];
-    if (self) {
-        _apiKey = apiKey;
-        _previousOffset = 0;
-    }
-    return self;
-}
-
-- (NSString *)loadAPIKey {
-    NSDictionary *environment = [[NSProcessInfo processInfo] environment];
-    NSString *envKey = environment[@"CURSEFORGE_API_KEY"];
-    if (!envKey || envKey.length == 0) {
-        NSLog(@"⚠️ WARNING: CurseForge API key missing!");
-    }
-    return envKey;
-}
-
-#pragma mark - Endpoint Request
-
-- (id)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params {
-    __block id result = nil;
-    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
-    
-    NSURLComponents *components = [NSURLComponents componentsWithString:
-       [NSString stringWithFormat:@"https://api.curseforge.com/v1/%@", endpoint]];
-    
-    // Build query
-    if ([params isKindOfClass:[NSDictionary class]]) {
-        NSMutableArray<NSURLQueryItem *> *queryItems = [NSMutableArray array];
-        for (NSString *key in params) {
-            [queryItems addObject:[NSURLQueryItem queryItemWithName:key value:[NSString stringWithFormat:@"%@", params[key]]]];
-        }
-        components.queryItems = queryItems;
-    }
-    
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:components.URL];
-    [request setValue:self.apiKey forHTTPHeaderField:@"x-api-key"];
-    [request setValue:@"Mozilla/5.0" forHTTPHeaderField:@"User-Agent"];
-    
-    NSURLSessionDataTask *task = [[NSURLSession sharedSession]
-        dataTaskWithRequest:request
-          completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
-    {
-        if (error) {
-            self.lastError = error;
-            dispatch_semaphore_signal(semaphore);
-            return;
-        }
-        
-        NSError *jsonError = nil;
-        id parsed = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
-        if (jsonError) {
-            self.lastError = jsonError;
-        } else {
-            result = parsed;
-        }
-        dispatch_semaphore_signal(semaphore);
-    }];
-    
-    [task resume];
-    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-    return result;
-}
-
-#pragma mark - Searching
-
-- (NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, id> *)searchFilters
-                      previousPageResult:(NSMutableArray *)previousResults
+- (void)installModpackFromDetail:(NSDictionary *)detail atIndex:(NSInteger)index
 {
-    NSMutableDictionary *params = [NSMutableDictionary dictionary];
-    params[@"gameId"] = @(kCurseForgeGameIDMinecraft);
-    
-    BOOL isModpack = [searchFilters[@"isModpack"] boolValue];
-    params[@"classId"] = isModpack ? @(kCurseForgeClassIDModpack) : @(kCurseForgeClassIDMod);
-    
-    // Safely extract "name"
-    NSString *searchName = ([searchFilters[@"name"] isKindOfClass:[NSString class]] ? searchFilters[@"name"] : @"");
-    params[@"searchFilter"] = searchName;
-    
-    params[@"sortField"] = @(1);
-    params[@"sortOrder"] = @"desc";
-    
-    int limit = 50;
-    params[@"pageSize"] = @(limit);
-    
-    // Check if new search
-    NSString *lastSearchName = ([self.lastSearchTerm isKindOfClass:[NSString class]] ? self.lastSearchTerm : @"");
-    if (!previousResults || ![searchName isEqualToString:lastSearchName]) {
-        self.previousOffset = 0;
-        self.reachedLastPage = NO;
-    }
-    
-    params[@"index"] = @(self.previousOffset);
-    
-    // Optionally specify a Minecraft version
-    if ([searchFilters[@"mcVersion"] isKindOfClass:[NSString class]] &&
-        [searchFilters[@"mcVersion"] length] > 0)
-    {
-        params[@"gameVersion"] = searchFilters[@"mcVersion"];
-    }
-    
-    NSDictionary *response = [self getEndpoint:@"mods/search" params:params];
-    if (!response || ![response[@"data"] isKindOfClass:[NSArray class]]) {
-        return nil; // self.lastError presumably set
-    }
-    
-    NSArray *dataArray = response[@"data"];
-    NSMutableArray *results = previousResults ?: [NSMutableArray array];
-    
-    self.previousOffset += dataArray.count;
-    self.reachedLastPage = dataArray.count < limit;
-    
-    for (NSDictionary *modData in dataArray) {
-        id modIdValue = modData[@"id"];
-        NSString *modId = [modIdValue respondsToSelector:@selector(stringValue)]
-                          ? [modIdValue stringValue] : @"";
-        
-        NSString *title   = ([modData[@"name"] isKindOfClass:[NSString class]]    ? modData[@"name"]    : @"");
-        NSString *summary = ([modData[@"summary"] isKindOfClass:[NSString class]] ? modData[@"summary"] : @"");
-        
-        NSDictionary *logoDict = ([modData[@"logo"] isKindOfClass:[NSDictionary class]] ? modData[@"logo"] : nil);
-        NSString *imageUrl = (logoDict && [logoDict[@"thumbnailUrl"] isKindOfClass:[NSString class]]
-                              ? logoDict[@"thumbnailUrl"]
-                              : @"");
-        
-        NSMutableDictionary *item = [@{
-            @"apiSource": @(0),
-            @"isModpack": @(isModpack),
-            @"id": modId,
-            @"title": title,
-            @"description": summary,
-            @"imageUrl": imageUrl
-        } mutableCopy];
-        
-        // Load version data right away
-        [self loadDetailsOfMod:item];
-        [results addObject:item];
-    }
-    
-    self.lastSearchTerm = searchName;
-    return results;
-}
-
-#pragma mark - Installing
-
-- (void)installModpackFromDetail:(NSDictionary *)detail atIndex:(NSInteger)index {
-    // Make sure detail has versionUrls array
+    // 1) Validate input
     if (![detail[@"versionUrls"] isKindOfClass:[NSArray class]]) {
+        NSLog(@"[CurseForgeAPI] No versionUrls array in detail!");
         return;
     }
-    if (index >= [detail[@"versionUrls"] count]) {
+    NSArray *urls = detail[@"versionUrls"];
+    if (index < 0 || index >= urls.count) {
+        NSLog(@"[CurseForgeAPI] Invalid index %ld for versionUrls!", (long)index);
         return;
     }
-    
-    NSString *zipUrlString = detail[@"versionUrls"][index];
-    if (!zipUrlString.length) {
+
+    NSString *zipUrlString = urls[index];
+    if (zipUrlString.length == 0) {
+        NSLog(@"[CurseForgeAPI] Empty URL at index %ld!", (long)index);
         return;
     }
-    
     NSURL *zipUrl = [NSURL URLWithString:zipUrlString];
-    NSString *destPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"modpack_install"];
-    [[NSFileManager defaultManager] createDirectoryAtPath:destPath
+    if (!zipUrl) {
+        NSLog(@"[CurseForgeAPI] Could not build NSURL from: %@", zipUrlString);
+        return;
+    }
+
+    NSString *tempDir = [NSTemporaryDirectory() stringByAppendingPathComponent:@"curseforge_install"];
+    [[NSFileManager defaultManager] createDirectoryAtPath:tempDir
                               withIntermediateDirectories:YES
                                                attributes:nil
                                                     error:nil];
-    
-    NSString *zipPath = [destPath stringByAppendingPathComponent:@"modpack.zip"];
-    NSURLSessionDownloadTask *downloadTask = [[NSURLSession sharedSession]
+    NSString *zipPath = [tempDir stringByAppendingPathComponent:@"modpack.zip"];
+
+    __block NSError *downloadError = nil;
+    dispatch_semaphore_t sem = dispatch_semaphore_create(0);
+
+    NSURLSessionDownloadTask *task = [[NSURLSession sharedSession]
        downloadTaskWithURL:zipUrl
          completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error)
     {
         if (error) {
-            NSLog(@"[CurseForgeAPI] Download error: %@", error.localizedDescription);
-            return;
+            downloadError = error;
+        } else {
+            // Move the downloaded temp file to zipPath
+            NSError *moveError = nil;
+            [[NSFileManager defaultManager] moveItemAtURL:location
+                                                    toURL:[NSURL fileURLWithPath:zipPath]
+                                                    error:&moveError];
+            if (moveError) {
+                downloadError = moveError;
+            }
         }
-        
-        NSError *moveError = nil;
-        [[NSFileManager defaultManager] moveItemAtURL:location
-                                                toURL:[NSURL fileURLWithPath:zipPath]
-                                                error:&moveError];
-        if (moveError) {
-            NSLog(@"Error moving downloaded file: %@", moveError.localizedDescription);
-            return;
-        }
-        
-        NSError *archiveError = nil;
-        UZKArchive *archive = [[UZKArchive alloc] initWithPath:zipPath error:&archiveError];
-        if (archiveError) {
-            NSLog(@"Failed to open modpack package: %@", archiveError.localizedDescription);
-            return;
-        }
-        
-        [archive extractFilesTo:destPath overwrite:YES error:&archiveError];
-        if (archiveError) {
-            NSLog(@"Failed to extract modpack: %@", archiveError.localizedDescription);
-            return;
-        }
-        
-        // Remove the original zip
-        [[NSFileManager defaultManager] removeItemAtPath:zipPath error:nil];
-        NSLog(@"Modpack installed successfully from CurseForge.");
+        dispatch_semaphore_signal(sem);
     }];
-    
-    [downloadTask resume];
-}
+    [task resume];
 
-#pragma mark - Loading Details
+    dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
 
-- (void)loadDetailsOfMod:(NSMutableDictionary *)item {
-    NSString *modId = item[@"id"];
-    if (!modId.length) {
+    if (downloadError) {
+        NSLog(@"[CurseForgeAPI] Download error: %@", downloadError.localizedDescription);
         return;
     }
-    
-    NSDictionary *response = [self getEndpoint:[NSString stringWithFormat:@"mods/%@", modId]
-                                        params:nil];
-    if (!response || ![response[@"data"] isKindOfClass:[NSDictionary class]]) {
+
+    NSError *archiveError = nil;
+    UZKArchive *archive = [[UZKArchive alloc] initWithPath:zipPath error:&archiveError];
+    if (archiveError) {
+        NSLog(@"[CurseForgeAPI] Failed to open modpack zip: %@", archiveError.localizedDescription);
         return;
     }
-    
-    NSDictionary *data = response[@"data"];
-    NSArray *files = data[@"latestFiles"];
-    if (![files isKindOfClass:[NSArray class]]) {
+    NSError *extractError = nil;
+    [archive extractFilesTo:tempDir overwrite:YES error:&extractError];
+    if (extractError) {
+        NSLog(@"[CurseForgeAPI] Failed to extract modpack: %@", extractError.localizedDescription);
         return;
     }
-    
-    NSMutableArray *versionNames    = [NSMutableArray array];
-    NSMutableArray *mcVersionNames  = [NSMutableArray array];
-    NSMutableArray *versionUrls     = [NSMutableArray array];
-    
-    for (NSDictionary *file in files) {
-        // Display name
-        NSString *displayName = ([file[@"displayName"] isKindOfClass:[NSString class]]
-                                 ? file[@"displayName"]
-                                 : @"");
-        [versionNames addObject:displayName];
-        
-        // Minecraft version(s)
-        NSArray *gameVersions = ([file[@"gameVersions"] isKindOfClass:[NSArray class]]
-                                 ? file[@"gameVersions"]
-                                 : @[]);
-        NSString *mcVersion = (gameVersions.count > 0 ? gameVersions.firstObject : @"");
-        [mcVersionNames addObject:mcVersion];
-        
-        // Download URL
-        id fileIdValue = file[@"id"];
-        NSString *fileId = ([fileIdValue respondsToSelector:@selector(stringValue)]
-                            ? [fileIdValue stringValue]
-                            : @"");
-        NSString *downloadUrl = ([file[@"downloadUrl"] isKindOfClass:[NSString class]]
-                                 ? file[@"downloadUrl"]
-                                 : @"");
-        
-        // If no direct downloadUrl, build it from the file ID
-        if (downloadUrl.length == 0 && fileId.length >= 4) {
-            NSString *fileName = ([file[@"fileName"] isKindOfClass:[NSString class]]
-                                  ? file[@"fileName"]
-                                  : @"");
-            // Common pattern for CF
-            downloadUrl = [NSString stringWithFormat:@"https://edge.forgecdn.net/files/%@/%@/%@",
-                           [fileId substringToIndex:4],
-                           [fileId substringWithRange:NSMakeRange(4, 2)],
-                           fileName];
+
+    NSString *manifestPath = [tempDir stringByAppendingPathComponent:@"manifest.json"];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:manifestPath]) {
+        NSLog(@"[CurseForgeAPI] No manifest.json found in the CF modpack!");
+        return;
+    }
+
+    NSData *manifestData = [NSData dataWithContentsOfFile:manifestPath];
+    if (!manifestData) {
+        NSLog(@"[CurseForgeAPI] Could not load manifest.json data!");
+        return;
+    }
+
+    NSError *jsonError = nil;
+    NSDictionary *manifest = [NSJSONSerialization JSONObjectWithData:manifestData options:0 error:&jsonError];
+    if (!manifest || jsonError) {
+        NSLog(@"[CurseForgeAPI] Could not parse manifest.json: %@", jsonError.localizedDescription);
+        return;
+    }
+
+    NSError *overridesError = nil;
+    [ModpackUtils archive:archive extractDirectory:@"overrides" toPath:tempDir error:&overridesError];
+    [ModpackUtils archive:archive extractDirectory:@"Overrides" toPath:tempDir error:&overridesError];
+    if (overridesError) {
+        NSLog(@"[CurseForgeAPI] Could not extract overrides: %@", overridesError.localizedDescription);
+    }
+
+    [[NSFileManager defaultManager] removeItemAtPath:zipPath error:nil];
+
+    NSString *packName = [manifest[@"name"] isKindOfClass:[NSString class]] ? manifest[@"name"] : @"CF_Pack";
+    NSString *finalInstallPath = [NSTemporaryDirectory() stringByAppendingPathComponent:packName];
+
+    NSError *moveAllError = nil;
+    [[NSFileManager defaultManager] moveItemAtPath:tempDir
+                                            toPath:finalInstallPath
+                                             error:&moveAllError];
+    if (moveAllError) {
+        NSLog(@"[CurseForgeAPI] Could not rename temp folder: %@", moveAllError.localizedDescription);
+        return;
+    }
+
+    NSDictionary *mcSection = manifest[@"minecraft"];
+    NSString *profileVersionID = @"";
+    if ([mcSection isKindOfClass:[NSDictionary class]]) {
+        NSArray *loaders = mcSection[@"modLoaders"];
+        if ([loaders isKindOfClass:[NSArray class]] && loaders.count > 0) {
+            NSDictionary *firstLoader = loaders.firstObject;
+            if ([firstLoader[@"id"] isKindOfClass:[NSString class]]) {
+                // e.g. "forge-xx.xx.xx"
+                profileVersionID = firstLoader[@"id"];
+            }
         }
-        
-        [versionUrls addObject:downloadUrl ?: @""];
     }
-    
-    item[@"versionNames"] = versionNames;
-    item[@"mcVersionNames"] = mcVersionNames;
-    item[@"versionUrls"] = versionUrls;
+
+    NSString *tmpIconPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"icon.png"];
+    NSData *iconData = [NSData dataWithContentsOfFile:tmpIconPath];
+    NSString *iconBase64 = iconData ? [iconData base64EncodedStringWithOptions:0] : @"";
+
+    PLProfiles *profiles = [PLProfiles current];
+    profiles.profiles[packName] = [@{
+        @"gameDir": [NSString stringWithFormat:@"./custom_gamedir/%@", finalInstallPath.lastPathComponent],
+        @"name": packName,
+        @"lastVersionId": profileVersionID ?: @"",
+        @"icon": [NSString stringWithFormat:@"data:image/png;base64,%@", iconBase64]
+    } mutableCopy];
+    profiles.selectedProfileName = packName;
+
+    NSLog(@"[CurseForgeAPI] Successfully installed CF modpack named: %@", packName);
 }
 
 @end
