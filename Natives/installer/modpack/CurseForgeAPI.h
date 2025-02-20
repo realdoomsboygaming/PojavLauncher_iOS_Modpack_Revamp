74a395fb85a11dfc524006e8e761811cda24a9fa
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-20 11:46:21 -0600

    Update CurseForgeAPI.h

--- Natives/installer/modpack/CurseForgeAPI.h
+++ Natives/installer/modpack/CurseForgeAPI.h
@@ -1,20 +1,25 @@
 #import <Foundation/Foundation.h>
 #import "ModpackAPI.h"
-NS_ASSUME_NONNULL_END
+
+NS_ASSUME_NONNULL_BEGIN
+
 @interface CurseForgeAPI : ModpackAPI
 
-- (instancetype)initWithAPIKey:(NSString *)apiKey;
+- (instancetype)initWithAPIKey:(NSString *)apiKey NS_DESIGNATED_INITIALIZER;
+
 - (void)searchModWithFilters:(NSDictionary *)searchFilters
          previousPageResult:(NSMutableArray *)prevResult
-                 completion:(void (^)(NSMutableArray * _Nullable results, NSError * _Nullable error))completion;
+                 completion:(void (^ _Nonnull)(NSMutableArray * _Nullable results, NSError * _Nullable error))completion;
+
 - (void)loadDetailsOfMod:(NSMutableDictionary *)item
-              completion:(void (^)(NSError * _Nullable error))completion;
+              completion:(void (^ _Nonnull)(NSError * _Nullable error))completion;
+
 - (void)installModpackFromDetail:(NSDictionary *)modDetail
                          atIndex:(NSUInteger)selectedVersion
-                      completion:(void (^)(NSError * _Nullable error))completion;
+                      completion:(void (^ _Nonnull)(NSError * _Nullable error))completion;
 
-/// The parent view controller is used for presenting dialogs (if needed)
-@property (nonatomic, weak) UIViewController *parentViewController;
+@property (nonatomic, weak, nullable) UIViewController *parentViewController;
 
 @end
+
 NS_ASSUME_NONNULL_END
