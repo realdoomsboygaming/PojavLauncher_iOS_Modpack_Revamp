ce71d506f5e6667913acb89c76c082d8064255d4
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-20 19:29:12 -0600

    Update FabricInstallViewController.m

--- Natives/installer/FabricInstallViewController.m
+++ Natives/installer/FabricInstallViewController.m
@@ -8,45 +8,34 @@
 #import "PLProfiles.h"
 #import "ios_uikit_bridge.h"
 #import "utils.h"
-#include <objc/runtime.h>
+#import <objc/runtime.h>
 
-@interface FabricInstallViewController()
-@property(nonatomic) NSDictionary *endpoints;
-@property(nonatomic) NSMutableDictionary *localKVO;
-// Loader metadata
-@property(nonatomic) NSArray<NSDictionary *> *loaderMetadata;
-@property(nonatomic) NSMutableArray<NSString *> *loaderList;
-// Game metadata
-@property(nonatomic) NSArray<NSDictionary *> *versionMetadata;
-@property(nonatomic) NSMutableArray<NSString *> *versionList;
+@interface FabricInstallViewController ()
+@property(nonatomic, strong) NSDictionary *endpoints;
+@property(nonatomic, strong) NSMutableDictionary *localKVO;
+@property(nonatomic, strong) NSArray<NSDictionary *> *loaderMetadata;
+@property(nonatomic, strong) NSMutableArray<NSString *> *loaderList;
+@property(nonatomic, strong) NSArray<NSDictionary *> *versionMetadata;
+@property(nonatomic, strong) NSMutableArray<NSString *> *versionList;
 @end
 
 @implementation FabricInstallViewController
 
 - (void)viewDidLoad {
-    // Setup navigation bar
+    [super viewDidLoad];
     self.title = localize(@"profile.title.install_fabric_quilt", nil);
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(actionDone:)];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemClose target:self action:@selector(actionClose)];
-
-    // Setup appearance
     self.prefSectionsVisible = YES;
-
-    // Setup preference getter and setter
     __weak __typeof(self) weakSelf = self;
-    self.localKVO = @{
-        @"gameVersion": @"1.20.1",
-        @"loaderVendor": @"Fabric",
-        @"loaderVersion": @"0.14.22"
-    }.mutableCopy;
-    self.getPreference = ^id(NSString *section, NSString *key){
+    self.localKVO = [@{@"gameVersion": @"1.20.1", @"loaderVendor": @"Fabric", @"loaderVersion": @"0.14.22"} mutableCopy];
+    self.getPreference = ^id(NSString *section, NSString *key) {
         return weakSelf.localKVO[key];
     };
-    self.setPreference = ^(NSString *section, NSString *key, NSString *value){
+    self.setPreference = ^(NSString *section, NSString *key, NSString *value) {
         weakSelf.localKVO[key] = value;
     };
-
-    id typePickSegment = ^void(UITableViewCell *cell, NSString *section, NSString *key, NSDictionary *item) {
+    id typePickSegment = ^(UITableViewCell *cell, NSString *section, NSString *key, NSDictionary *item) {
         UISegmentedControl *view = [[UISegmentedControl alloc] initWithItems:item[@"pickList"]];
         [view addTarget:weakSelf action:@selector(segmentChanged:) forControlEvents:UIControlEventValueChanged];
         if (view.selectedSegmentIndex == UISegmentedControlNoSegment) {
@@ -54,7 +43,6 @@
         }
         cell.accessoryView = view;
     };
-
     self.versionList = [NSMutableArray new];
     self.loaderList = [NSMutableArray new];
     self.prefContents = @[
@@ -89,7 +77,6 @@
               @"title": @"preference.profile.title.loader_type",
               @"type": typePickSegment,
               @"pickList": @[localize(@"Release", nil), @"Unstable"],
-              //localize(@"Unstable", nil)
               @"action": ^(int type) {
                   [weakSelf changeLoaderTypeTo:type];
               }
@@ -103,35 +90,26 @@
             }
         ]
     ];
-
-    // Ensure views are loaded here
-    [super viewDidLoad];
-
-    // Init endpoint info
-    self.endpoints = FabricUtils.endpoints;
+    self.endpoints = [FabricUtils endpoints];
     [self fetchVersionEndpoints:0];
 }
 
 - (void)fetchVersionEndpoints:(int)type {
-    // Fetch version
     __block BOOL errorShown = NO;
     id errorCallback = ^(NSURLSessionTask *operation, NSError *error) {
         if (!errorShown) {
             errorShown = YES;
-            NSDebugLog(@"Error: %@", error);
             showDialog(localize(@"Error", nil), error.localizedDescription);
             [self actionClose];
         }
     };
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     NSDictionary *endpoint = self.endpoints[self.localKVO[@"loaderVendor"]];
-    [manager GET:endpoint[@"game"] parameters:nil headers:nil progress:nil  success:^(NSURLSessionTask *task, NSArray *response) {
-        NSDebugLog(@"[%@ Installer] Got %d game versions", self.localKVO[@"loaderVendor"], response.count);
+    [manager GET:endpoint[@"game"] parameters:nil headers:nil progress:nil success:^(NSURLSessionTask *task, NSArray *response) {
         self.versionMetadata = response;
         [self changeVersionTypeTo:[self.localKVO[@"gameType_index"] intValue]];
     } failure:errorCallback];
     [manager GET:endpoint[@"loader"] parameters:nil headers:nil progress:nil success:^(NSURLSessionTask *task, NSArray *response) {
-        NSDebugLog(@"[%@ Installer] Got %d loader versions", self.localKVO[@"loaderVendor"], response.count);
         self.loaderMetadata = response;
         [self changeLoaderTypeTo:[self.localKVO[@"loaderType_index"] intValue]];
     } failure:errorCallback];
@@ -143,49 +121,34 @@
 
 - (void)actionDone:(UIBarButtonItem *)sender {
     sender.enabled = NO;
-
     NSDictionary *endpoint = self.endpoints[self.localKVO[@"loaderVendor"]];
     NSString *path = [NSString stringWithFormat:endpoint[@"json"], self.localKVO[@"gameVersion"], self.localKVO[@"loaderVersion"]];
-    NSDebugLog(@"[%@ Installer] Downloading %@", self.localKVO[@"loaderVendor"], path);
-
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    [manager GET:path parameters:nil headers:nil progress:nil  success:^(NSURLSessionTask *task, NSDictionary *response) {
+    [manager GET:path parameters:nil headers:nil progress:nil success:^(NSURLSessionTask *task, NSDictionary *response) {
         sender.enabled = YES;
-
-        NSString *jsonPath = [NSString stringWithFormat:@"%1$s/versions/%2$@/%2$@.json", getenv("POJAV_GAME_DIR"), response[@"id"]];
-        [NSFileManager.defaultManager createDirectoryAtPath:jsonPath.stringByDeletingLastPathComponent withIntermediateDirectories:YES attributes:nil error:nil];
+        NSString *jsonPath = [NSString stringWithFormat:@"%s/versions/%@/%@.json", getenv("POJAV_GAME_DIR"), response[@"id"], response[@"id"]];
+        [[NSFileManager defaultManager] createDirectoryAtPath:jsonPath.stringByDeletingLastPathComponent withIntermediateDirectories:YES attributes:nil error:nil];
         NSError *error = saveJSONToFile(response, jsonPath);
         if (error) {
             showDialog(localize(@"Error", nil), error.localizedDescription);
         } else {
-            [localVersionList addObject:@{
-                @"id": response[@"id"],
-                @"type": @"custom"}];
-            // Jump to the profile editor
+            [localVersionList addObject:@{@"id": response[@"id"], @"type": @"custom"}];
             LauncherProfileEditorViewController *vc = [LauncherProfileEditorViewController new];
-            vc.profile = @{
-                @"icon": endpoint[@"icon"],
-                @"name": response[@"id"],
-                @"lastVersionId": response[@"id"]
-            }.mutableCopy;
+            vc.profile = [@{@"icon": endpoint[@"icon"], @"name": response[@"id"], @"lastVersionId": response[@"id"]} mutableCopy];
             [self.navigationController pushViewController:vc animated:YES];
         }
     } failure:^(NSURLSessionTask *operation, NSError *error) {
         sender.enabled = YES;
-        NSDebugLog(@"Error: %@", error);
         showDialog(localize(@"Error", nil), error.localizedDescription);
     }];
 }
 
 - (void)changeTypeToStable:(BOOL)stable forList:(NSMutableArray *)list fromMetadata:(NSArray *)metadata atRow:(int)row key:(NSString *)key {
     [list removeAllObjects];
-
     for (NSDictionary *version in metadata) {
         if (version[@"stable"]) {
-            // Fabric: has stable key
             if ([version[@"stable"] boolValue] != stable) continue;
         } else {
-            // Quilt: has beta in the version name
             if ([version[@"version"] containsString:@"beta"] == stable) continue;
         }
         [list addObject:version[@"version"]];
@@ -193,21 +156,26 @@
     self.localKVO[key] = list.firstObject;
     [self.tableView reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:row inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
 }
+
 - (void)changeLoaderTypeTo:(int)type {
-    [self changeTypeToStable:type==0 forList:self.loaderList fromMetadata:self.loaderMetadata atRow:4 key:@"loaderVersion"];
+    [self changeTypeToStable:(type == 0) forList:self.loaderList fromMetadata:self.loaderMetadata atRow:4 key:@"loaderVersion"];
 }
+
 - (void)changeVersionTypeTo:(int)type {
-    [self changeTypeToStable:type==0 forList:self.versionList fromMetadata:self.versionMetadata atRow:1 key:@"gameVersion"];
+    [self changeTypeToStable:(type == 0) forList:self.versionList fromMetadata:self.versionMetadata atRow:1 key:@"gameVersion"];
 }
 
 - (void)segmentChanged:(UISegmentedControl *)sender {
     NSDictionary *item = objc_getAssociatedObject(sender, @"item");
     self.localKVO[item[@"key"]] = [sender titleForSegmentAtIndex:sender.selectedSegmentIndex];
-    self.localKVO[[item[@"key"] stringByAppendingString:@"_index"]] = @(sender.selectedSegmentIndex);
-    void(^invokeAction)(int selected) = item[@"action"];
+    self.localKVO[[NSString stringWithFormat:@"%@_index", item[@"key"]]] = @(sender.selectedSegmentIndex);
+    void(^invokeAction)(int) = item[@"action"];
     if (invokeAction) {
         invokeAction(sender.selectedSegmentIndex);
     }
+}
+
+- (void)updateSearchResults {
 }
 
 @end
