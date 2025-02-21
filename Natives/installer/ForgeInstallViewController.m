f6a587a6ca64c7e8ac7e9e09a7e94ed941d12c51
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-20 19:30:20 -0600

    Update ForgeInstallViewController.m

--- Natives/installer/ForgeInstallViewController.m
+++ Natives/installer/ForgeInstallViewController.m
@@ -4,17 +4,16 @@
 #import "WFWorkflowProgressView.h"
 #import "ios_uikit_bridge.h"
 #import "utils.h"
-#include <dlfcn.h>
+#import <dlfcn.h>
 
-@interface ForgeInstallViewController()<NSXMLParserDelegate>
-@property(atomic) AFURLSessionManager *afManager;
-@property(nonatomic) WFWorkflowProgressView *progressView;
-
-@property(nonatomic) NSDictionary *endpoints;
-@property(nonatomic) NSMutableArray<NSNumber *> *visibilityList;
-@property(nonatomic) NSMutableArray<NSString *> *versionList;
-@property(nonatomic) NSMutableArray<NSMutableArray *> *forgeList;
-@property(nonatomic, assign) BOOL isVersionElement;
+@interface ForgeInstallViewController () <NSXMLParserDelegate>
+@property (nonatomic, strong) AFURLSessionManager *afManager;
+@property (nonatomic, strong) WFWorkflowProgressView *progressView;
+@property (nonatomic, strong) NSDictionary *endpoints;
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *visibilityList;
+@property (nonatomic, strong) NSMutableArray<NSString *> *versionList;
+@property (nonatomic, strong) NSMutableArray<NSMutableArray *> *forgeList;
+@property (nonatomic, assign) BOOL isVersionElement;
 @end
 
 @implementation ForgeInstallViewController
@@ -25,24 +24,11 @@
     segment.selectedSegmentIndex = 0;
     [segment addTarget:self action:@selector(segmentChanged:) forControlEvents:UIControlEventValueChanged];
     self.navigationItem.titleView = segment;
-
-    // Load WFWorkflowProgressView
     dlopen("/System/Library/PrivateFrameworks/WorkflowUIServices.framework/WorkflowUIServices", RTLD_GLOBAL);
     self.progressView = [[NSClassFromString(@"WFWorkflowProgressView") alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
     self.progressView.resolvedTintColor = self.view.tintColor;
-    [self.progressView addTarget:self
-        action:@selector(actionCancelDownload) forControlEvents:UIControlEventTouchUpInside];
-
-    self.endpoints = @{
-        @"Forge": @{
-            @"installer": @"https://maven.minecraftforge.net/net/minecraftforge/forge/%1$@/forge-%1$@-installer.jar",
-            @"metadata": @"https://maven.minecraftforge.net/net/minecraftforge/forge/maven-metadata.xml"
-        },
-        @"NeoForge": @{
-            @"installer": @"https://maven.neoforged.net/net/neoforged/forge/%1$@/forge-%1$@-installer.jar",
-            @"metadata": @"https://maven.neoforged.net/releases/net/neoforged/forge/maven-metadata.xml"
-        }
-    };
+    [self.progressView addTarget:self action:@selector(actionCancelDownload) forControlEvents:UIControlEventTouchUpInside];
+    self.endpoints = @{@"Forge": @{@"installer": @"https://maven.minecraftforge.net/net/minecraftforge/forge/%@/forge-%@-installer.jar", @"metadata": @"https://maven.minecraftforge.net/net/minecraftforge/forge/maven-metadata.xml"}, @"NeoForge": @{@"installer": @"https://maven.neoforged.net/net/neoforged/forge/%@/forge-%@-installer.jar", @"metadata": @"https://maven.neoforged.net/releases/net/neoforged/forge/maven-metadata.xml"}};
     self.visibilityList = [NSMutableArray new];
     self.versionList = [NSMutableArray new];
     self.forgeList = [NSMutableArray new];
@@ -60,7 +46,7 @@
 - (void)loadMetadataFromVendor:(NSString *)vendor {
     [self switchToLoadingState];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        NSURL *url = [[NSURL alloc] initWithString:self.endpoints[vendor][@"metadata"]];
+        NSURL *url = [NSURL URLWithString:self.endpoints[vendor][@"metadata"]];
         NSXMLParser *parser = [[NSXMLParser alloc] initWithContentsOfURL:url];
         parser.delegate = self;
         if (![parser parse]) {
@@ -95,35 +81,34 @@
     [self loadMetadataFromVendor:vendor];
 }
 
-#pragma mark UITableViewDataSource
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return self.versionList.count;
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    UITableViewHeaderFooterView *view = [self.tableView dequeueReusableHeaderFooterViewWithIdentifier:@"section"];
+    UITableViewHeaderFooterView *view = [tableView dequeueReusableHeaderFooterViewWithIdentifier:@"section"];
     if (!view) {
         view = [[UITableViewHeaderFooterView alloc] initWithReuseIdentifier:@"section"];
         UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tableViewDidSelectSection:)];
         [view addGestureRecognizer:tapGesture];
     }
+    view.textLabel.text = self.versionList[section];
     return view;
+}
+
+- (void)tableViewDidSelectSection:(UITapGestureRecognizer *)sender {
+    UITableViewHeaderFooterView *view = (id)sender.view;
+    NSInteger section = [self.versionList indexOfObject:view.textLabel.text];
+    self.visibilityList[section] = @(![self.visibilityList[section] boolValue]);
+    [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:section] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     return self.versionList[section];
 }
 
-- (void)tableViewDidSelectSection:(UITapGestureRecognizer *)sender {
-    UITableViewHeaderFooterView *view = (id)sender.view;
-    int section = [self.versionList indexOfObject:view.textLabel.text];
-    self.visibilityList[section] = @(!self.visibilityList[section].boolValue);
-    [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:section] withRowAnimation:UITableViewRowAnimationAutomatic];
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.visibilityList[section].boolValue ? self.forgeList[section].count : 0;
+    return [self.visibilityList[section] boolValue] ? [self.forgeList[section] count] : 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -131,7 +116,6 @@
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
     }
-
     cell.textLabel.text = self.forgeList[indexPath.section][indexPath.row];
     return cell;
 }
@@ -139,27 +123,22 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
     tableView.allowsSelection = NO;
-
     [self switchToLoadingState];
     self.progressView.fractionCompleted = 0;
-
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     cell.accessoryView = self.progressView;
-
     UISegmentedControl *segment = (id)self.navigationItem.titleView;
     NSString *vendor = [segment titleForSegmentAtIndex:segment.selectedSegmentIndex];
-    NSString *jarURL = [NSString stringWithFormat:self.endpoints[vendor][@"installer"], cell.textLabel.text];
+    NSString *jarURL = [NSString stringWithFormat:self.endpoints[vendor][@"installer"], cell.textLabel.text, cell.textLabel.text];
     NSString *outPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"tmp.jar"];
-    NSDebugLog(@"[Forge Installer] Downloading %@", jarURL);
-
     self.afManager = [AFURLSessionManager new];
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:jarURL]];
-    NSURLSessionDownloadTask *downloadTask = [self.afManager downloadTaskWithRequest:request progress:^(NSProgress * _Nonnull progress){
+    NSURLSessionDownloadTask *downloadTask = [self.afManager downloadTaskWithRequest:request progress:^(NSProgress * _Nonnull progress) {
         dispatch_async(dispatch_get_main_queue(), ^{
             self.progressView.fractionCompleted = progress.fractionCompleted;
         });
     } destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
-        [NSFileManager.defaultManager removeItemAtPath:outPath error:nil];
+        [[NSFileManager defaultManager] removeItemAtPath:outPath error:nil];
         return [NSURL fileURLWithPath:outPath];
     } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -167,7 +146,6 @@
             cell.accessoryView = nil;
             if (error) {
                 if (error.code != NSURLErrorCancelled) {
-                    NSDebugLog(@"Error: %@", error);
                     showDialog(localize(@"Error", nil), error.localizedDescription);
                 }
                 [self switchToReadyState];
@@ -179,36 +157,17 @@
             }];
         });
     }];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        [downloadTask resume];
-    });
+    [downloadTask resume];
 }
-
-- (void)addVersionToList:(NSString *)version {
-    if (![version containsString:@"-"]) {
-        return;
-    }
-    NSRange range = [version rangeOfString:@"-"];
-    NSString *gameVersion = [version substringToIndex:range.location];
-    //NSString *forgeVersion = [version substringFromIndex:range.location + 1];
-    if (![self.versionList containsObject:gameVersion]) {
-        [self.visibilityList addObject:@(NO)];
-        [self.versionList addObject:gameVersion];
-        [self.forgeList addObject:[NSMutableArray new]];
-    }
-    [self.forgeList.lastObject addObject:version];
-}
-
-#pragma mark NSXMLParser
 
 - (void)parserDidEndDocument:(NSXMLParser *)unused {
-        dispatch_async(dispatch_get_main_queue(), ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
         [self switchToReadyState];
         [self.tableView reloadData];
     });
 }
 
-- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName attributes:(NSDictionary *)attributeDict {
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary<NSString *, NSString *> *)attributeDict {
     self.isVersionElement = [elementName isEqualToString:@"version"];
 }
 
@@ -216,6 +175,18 @@
     if (self.isVersionElement) {
         [self addVersionToList:version];
     }
+}
+
+- (void)addVersionToList:(NSString *)version {
+    if (![version containsString:@"-"]) return;
+    NSRange range = [version rangeOfString:@"-"];
+    NSString *gameVersion = [version substringToIndex:range.location];
+    if (![self.versionList containsObject:gameVersion]) {
+        [self.visibilityList addObject:@(NO)];
+        [self.versionList addObject:gameVersion];
+        [self.forgeList addObject:[NSMutableArray new]];
+    }
+    [[self.forgeList lastObject] addObject:version];
 }
 
 @end
