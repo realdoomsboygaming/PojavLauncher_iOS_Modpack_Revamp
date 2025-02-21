3b907b94ea4bd3d1c1650fbb25aa3f3ecdc159b8
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-21 12:27:23 -0600

    Update DownloadProgressViewController.m

--- Natives/DownloadProgressViewController.m
+++ Natives/DownloadProgressViewController.m
@@ -2,7 +2,7 @@
 #import <objc/runtime.h>
 #import "DownloadProgressViewController.h"
 
-// Use static const pointers as keys for associated objects.
+// Use static const pointers for associated object keys.
 static const void *kProgressKey = &kProgressKey;
 static const void *kCellKey = &kCellKey;
 static void *CellProgressObserverContext = &CellProgressObserverContext;
@@ -27,11 +27,12 @@ static void *TotalProgressObserverContext = &TotalProgressObserverContext;
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]
          initWithBarButtonSystemItem:UIBarButtonSystemItemClose target:self action:@selector(actionClose)];
     self.tableView.allowsSelection = YES;
-    // Cells will use accessoryType (checkmark) instead of a custom progress view.
+    // We use accessoryType for the checkmark; no custom accessory view.
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
+    // Observe overall progress updates.
     [self.task.textProgress addObserver:self
                              forKeyPath:@"fractionCompleted"
                                 options:(NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew)
@@ -43,6 +44,17 @@ static void *TotalProgressObserverContext = &TotalProgressObserverContext;
     @try {
         [self.task.textProgress removeObserver:self forKeyPath:@"fractionCompleted"];
     } @catch (NSException *exception) {}
+}
+
+// Remove observers from cells when they go off-screen.
+- (void)tableView:(UITableView *)tableView didEndDisplayingCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSProgress *progress = objc_getAssociatedObject(cell, kProgressKey);
+    if (progress) {
+        @try {
+            [progress removeObserver:self forKeyPath:@"fractionCompleted"];
+        } @catch (NSException *exception) {}
+        objc_setAssociatedObject(cell, kProgressKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
 }
 
 - (void)actionClose {
@@ -88,12 +100,14 @@ static void *TotalProgressObserverContext = &TotalProgressObserverContext;
     
     // Safely obtain the file name.
     NSString *fileName = @"";
-    if (indexPath.row < self.task.fileList.count) {
-       fileName = self.task.fileList[indexPath.row];
+    @synchronized(self.task.fileList) {
+        if (indexPath.row < self.task.fileList.count) {
+            fileName = self.task.fileList[indexPath.row];
+        }
     }
     cell.textLabel.text = fileName;
     
-    // Remove any previous observer and associated progress.
+    // Remove any previous observer associated with this cell.
     NSProgress *oldProgress = objc_getAssociatedObject(cell, kProgressKey);
     if (oldProgress) {
        @try {
@@ -102,16 +116,19 @@ static void *TotalProgressObserverContext = &TotalProgressObserverContext;
        objc_setAssociatedObject(cell, kProgressKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
     
-    // Obtain NSProgress for this row, or create a dummy if missing.
+    // Obtain NSProgress for this row in a thread-safe manner.
     NSProgress *progress = nil;
-    if (indexPath.row < self.task.progressList.count) {
-       progress = self.task.progressList[indexPath.row];
-    } else {
+    @synchronized(self.task.progressList) {
+        if (indexPath.row < self.task.progressList.count) {
+            progress = self.task.progressList[indexPath.row];
+        }
+    }
+    if (!progress) {
        progress = [NSProgress progressWithTotalUnitCount:1];
        progress.completedUnitCount = 0;
     }
     
-    // Associate the progress with the cell using our static key.
+    // Associate progress with the cell.
     objc_setAssociatedObject(cell, kProgressKey, progress, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     objc_setAssociatedObject(progress, kCellKey, cell, OBJC_ASSOCIATION_ASSIGN);
     [progress addObserver:self forKeyPath:@"fractionCompleted"
