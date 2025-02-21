57b686cf9d89dfb54352391c1397df60931d2595
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-21 15:12:39 -0600

    Update MinecraftResourceDownloadTask.h

--- Natives/MinecraftResourceDownloadTask.h
+++ Natives/MinecraftResourceDownloadTask.h
@@ -3,15 +3,14 @@
 @class ModpackAPI;
 
 @interface MinecraftResourceDownloadTask : NSObject
-@property NSProgress *progress, *textProgress;
-@property NSMutableArray *fileList, *progressList;
-@property NSMutableDictionary* metadata;
-@property(nonatomic, copy) void(^handleError)(void);
+@property (nonatomic, strong) NSProgress *progress, *textProgress;
+@property (nonatomic, strong) NSMutableArray *fileList, *progressList;
+@property (nonatomic, strong) NSMutableDictionary *metadata;
+@property (nonatomic, copy) void(^handleError)(void);
 
 - (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url size:(NSUInteger)size sha:(NSString *)sha altName:(NSString *)altName toPath:(NSString *)path;
 - (void)finishDownloadWithErrorString:(NSString *)error;
 
 - (void)downloadVersion:(NSDictionary *)version;
 - (void)downloadModpackFromAPI:(ModpackAPI *)api detail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion;
-
 @end
