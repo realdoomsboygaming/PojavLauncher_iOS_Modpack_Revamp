144adccd9bcefb40fd652ce2329f4379e39fdfb6
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-21 08:50:19 -0600

    Add files via upload

---
+++ Natives/installer/Swift-Bridging-Header.h
@@ -0,0 +1,16 @@
+#ifndef Swift_Bridging_Header_h
+#define Swift_Bridging_Header_h
+
+// Import UnzipKit for UZKArchive support
+#import <UnzipKit/UnzipKit.h>
+
+// Import MinecraftResourceDownloadTask for download operations
+#import "MinecraftResourceDownloadTask.h"
+
+// Import PLProfiles for profile management
+#import "PLProfiles.h"
+
+//#import "ModpackAPI.h"
+//#import "FabricUtils.h"
+
+#endif 
