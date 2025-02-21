7652f5e682030744d281eb116b4761cd1a8179a4
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-21 10:56:34 -0600

    Update DownloadProgressViewController.m
    Update Implementation for Curseforge Support

--- Natives/DownloadProgressViewController.m
+++ Natives/DownloadProgressViewController.m
@@ -7,52 +7,58 @@ static void *CellProgressObserverContext = &CellProgressObserverContext;
 static void *TotalProgressObserverContext = &TotalProgressObserverContext;
 
 @interface DownloadProgressViewController ()
-@property NSInteger fileListCount;
+@property (nonatomic) NSInteger fileListCount;
 @end
 
 @implementation DownloadProgressViewController
 
 - (instancetype)initWithTask:(MinecraftResourceDownloadTask *)task {
     self = [super init];
-    self.task = task;
+    if (self) {
+        self.task = task;
+    }
     return self;
 }
 
 - (void)loadView {
     [super loadView];
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemClose target:self action:@selector(actionClose)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]
+        initWithBarButtonSystemItem:UIBarButtonSystemItemClose target:self action:@selector(actionClose)];
     self.tableView.allowsSelection = NO;
-
-    // Load WFWorkflowProgressView
+    
+    // Load WFWorkflowProgressView (private framework)
     dlopen("/System/Library/PrivateFrameworks/WorkflowUIServices.framework/WorkflowUIServices", RTLD_GLOBAL);
 }
+
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    
-[self.task.textProgress addObserver:self
-        forKeyPath:@"fractionCompleted"
-        options:NSKeyValueObservingOptionInitial
-        context:TotalProgressObserverContext];
+    // Observe overall progress updates
+    [self.task.textProgress addObserver:self
+                             forKeyPath:@"fractionCompleted"
+                                options:(NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew)
+                                context:TotalProgressObserverContext];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
     [super viewDidDisappear:animated];
-    
-[self.task.textProgress removeObserver:self forKeyPath:@"fractionCompleted"];
+    @try {
+        [self.task.textProgress removeObserver:self forKeyPath:@"fractionCompleted"];
+    } @catch (NSException *exception) {}
 }
 
 - (void)actionClose {
     [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    NSProgress *progress = object;
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
     if (context == CellProgressObserverContext) {
+        NSProgress *progress = object;
         UITableViewCell *cell = objc_getAssociatedObject(progress, @"cell");
         if (!cell) return;
         dispatch_async(dispatch_get_main_queue(), ^{
-            cell.detailTextLabel.text = progress.localizedAdditionalDescription;
-            WFWorkflowProgressView *progressView = (id)cell.accessoryView;
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%.0f%%", progress.fractionCompleted * 100];
+            WFWorkflowProgressView *progressView = (WFWorkflowProgressView *)cell.accessoryView;
             progressView.fractionCompleted = progress.fractionCompleted;
             if (progress.finished) {
                 [progressView transitionCompletedLayerToVisible:YES animated:YES haptic:NO];
@@ -60,7 +66,7 @@ static void *TotalProgressObserverContext = &TotalProgressObserverContext;
         });
     } else if (context == TotalProgressObserverContext) {
         dispatch_async(dispatch_get_main_queue(), ^{
-            self.title = progress.localizedDescription;
+            self.title = [NSString stringWithFormat:@"Downloading: %.0f%%", self.task.textProgress.fractionCompleted * 100];
             if (self.fileListCount != self.task.fileList.count) {
                 [self.tableView reloadData];
             }
@@ -71,50 +77,50 @@ static void *TotalProgressObserverContext = &TotalProgressObserverContext;
     }
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
+#pragma mark - UITableView DataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.task.fileList.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
-
-    if (cell == nil) {
+    
+    if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"];
         WFWorkflowProgressView *progressView = [[NSClassFromString(@"WFWorkflowProgressView") alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
         progressView.resolvedTintColor = self.view.tintColor;
         progressView.stopSize = 0;
         cell.accessoryView = progressView;
     }
-
-    // Unset the last cell displaying the progress
-    NSProgress *lastProgress = objc_getAssociatedObject(cell, @"progress");
-    if (lastProgress) {
-        objc_setAssociatedObject(lastProgress, @"cell", nil, OBJC_ASSOCIATION_ASSIGN);
+    
+    // Remove any previous observer from the cell's associated progress.
+    NSProgress *oldProgress = objc_getAssociatedObject(cell, @"progress");
+    if (oldProgress) {
+        objc_setAssociatedObject(oldProgress, @"cell", nil, OBJC_ASSOCIATION_ASSIGN);
         @try {
-            [lastProgress removeObserver:self forKeyPath:@"fractionCompleted"];
-        } @catch(id anException) {}
+            [oldProgress removeObserver:self forKeyPath:@"fractionCompleted"];
+        } @catch (NSException *exception) {}
     }
-
+    
     NSProgress *progress = self.task.progressList[indexPath.row];
     objc_setAssociatedObject(cell, @"progress", progress, OBJC_ASSOCIATION_ASSIGN);
     objc_setAssociatedObject(progress, @"cell", cell, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    [progress addObserver:self
-        forKeyPath:@"fractionCompleted"
-        options:NSKeyValueObservingOptionInitial
-        context:CellProgressObserverContext];
-
-    WFWorkflowProgressView *progressView = (id)cell.accessoryView;
-    if (lastProgress.finished) {
+    [progress addObserver:self forKeyPath:@"fractionCompleted"
+                options:(NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew)
+                context:CellProgressObserverContext];
+    
+    WFWorkflowProgressView *progressView = (WFWorkflowProgressView *)cell.accessoryView;
+    if (progress.finished) {
         [progressView reset];
     }
     progressView.fractionCompleted = progress.fractionCompleted;
     [progressView transitionCompletedLayerToVisible:progress.finished animated:NO haptic:NO];
     [progressView transitionRunningLayerToVisible:!progress.finished animated:NO];
-
+    
     cell.textLabel.text = self.task.fileList[indexPath.row];
-    cell.detailTextLabel.text = progress.localizedAdditionalDescription;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%.0f%%", progress.fractionCompleted * 100];
+    
     return cell;
 }
 
