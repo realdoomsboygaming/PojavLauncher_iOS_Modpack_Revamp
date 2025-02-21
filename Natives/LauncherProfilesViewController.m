dcb93d4934c8a3dcf7b0f32736f63c5617a30741
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-21 14:16:12 -0600

    Update LauncherProfilesViewController.m
    Fix removing profiles not deleting downloaded files

--- Natives/LauncherProfilesViewController.m
+++ Natives/LauncherProfilesViewController.m
@@ -21,7 +21,6 @@ typedef NS_ENUM(NSUInteger, LauncherProfilesTableSection) {
 };
 
 @interface LauncherProfilesViewController () //<UIContextMenuInteractionDelegate>
-
 @property(nonatomic) UIBarButtonItem *createButtonItem;
 @end
 
@@ -37,55 +36,42 @@ typedef NS_ENUM(NSUInteger, LauncherProfilesTableSection) {
     return @"MenuProfiles";
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     UIMenu *createMenu = [UIMenu menuWithTitle:localize(@"profile.title.create", nil) image:nil identifier:nil
-    options:UIMenuOptionsDisplayInline
-    children:@[
-        [UIAction
-            actionWithTitle:@"Vanilla" image:nil
-            identifier:@"vanilla" handler:^(UIAction *action) {
-                [self actionEditProfile:@{
-                    @"name": @"",
-                    @"lastVersionId": @"latest-release"}];
-            }],
+                                       options:UIMenuOptionsDisplayInline
+                                      children:@[
+        [UIAction actionWithTitle:@"Vanilla" image:nil identifier:@"vanilla" handler:^(UIAction *action) {
+            [self actionEditProfile:@{ @"name": @"", @"lastVersionId": @"latest-release"}];
+        }],
 #if 0 // TODO
-        [UIAction
-            actionWithTitle:@"OptiFine" image:nil
-            identifier:@"optifine" handler:createHandler],
+        [UIAction actionWithTitle:@"OptiFine" image:nil identifier:@"optifine" handler:createHandler],
 #endif
-        [UIAction
-            actionWithTitle:@"Fabric/Quilt" image:nil
-            identifier:@"fabric_or_quilt" handler:^(UIAction *action) {
-                [self actionCreateFabricProfile];
-            }],
-        [UIAction
-            actionWithTitle:@"Forge" image:nil
-            identifier:@"forge" handler:^(UIAction *action) {
-                [self actionCreateForgeProfile];
-            }],
-        [UIAction
-            actionWithTitle:@"Modpack" image:nil
-            identifier:@"modpack" handler:^(UIAction *action) {
-                [self actionCreateModpackProfile];
-            }]
+        [UIAction actionWithTitle:@"Fabric/Quilt" image:nil identifier:@"fabric_or_quilt" handler:^(UIAction *action) {
+            [self actionCreateFabricProfile];
+        }],
+        [UIAction actionWithTitle:@"Forge" image:nil identifier:@"forge" handler:^(UIAction *action) {
+            [self actionCreateForgeProfile];
+        }],
+        [UIAction actionWithTitle:@"Modpack" image:nil identifier:@"modpack" handler:^(UIAction *action) {
+            [self actionCreateModpackProfile];
+        }]
     ]];
     self.createButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd menu:createMenu];
-
+    
     self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleInsetGrouped];
     self.tableView.keyboardDismissMode = UIScrollViewKeyboardDismissModeInteractive;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-
+    
     // Put navigation buttons back in place
     self.navigationItem.rightBarButtonItems = @[[sidebarViewController drawAccountButton], self.createButtonItem];
-
+    
     // Pickup changes made in the profile editor and switching instance
-    [PLProfiles updateCurrent];
+    [PLProfiles.current updateCurrent];
     [self.tableView reloadData];
     [self.navigationController performSelector:@selector(reloadProfileList)];
 }
@@ -120,11 +106,10 @@ typedef NS_ENUM(NSUInteger, LauncherProfilesTableSection) {
 
 - (void)presentNavigatedViewController:(UIViewController *)vc {
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
-    //nav.navigationBar.prefersLargeTitles = YES;
     [self presentViewController:nav animated:YES completion:nil];
 }
 
-#pragma mark Table view
+#pragma mark - Table view
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 2;
@@ -171,17 +156,16 @@ typedef NS_ENUM(NSUInteger, LauncherProfilesTableSection) {
 
 - (void)setupProfileCell:(UITableViewCell *) cell atRow:(NSInteger)row {
     NSMutableDictionary *profile = PLProfiles.current.profiles.allValues[row];
-
+    
     cell.textLabel.text = profile[@"name"];
     cell.detailTextLabel.text = profile[@"lastVersionId"];
     cell.imageView.layer.magnificationFilter = kCAFilterNearest;
-
+    
     UIImage *fallbackImage = [[UIImage imageNamed:@"DefaultProfile"] _imageWithSize:CGSizeMake(40, 40)];
     [cell.imageView setImageWithURL:[NSURL URLWithString:profile[@"icon"]] placeholderImage:fallbackImage];
 }
 
-- (UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     NSString *cellID = indexPath.section == kInstances ? @"InstanceCell" : @"ProfileCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
     if (cell == nil) {
@@ -198,48 +182,63 @@ typedef NS_ENUM(NSUInteger, LauncherProfilesTableSection) {
         cell.userInteractionEnabled = YES;
         cell.accessoryView = nil;
     }
-
+    
     if (indexPath.section == kInstances) {
         [self setupInstanceCell:cell atRow:indexPath.row];
     } else {
         [self setupProfileCell:cell atRow:indexPath.row];
     }
-
+    
     cell.textLabel.enabled = cell.detailTextLabel.enabled = cell.userInteractionEnabled;
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
-
+    
     if (indexPath.section == kInstances) {
         if (indexPath.row == 0) {
             [self.navigationController pushViewController:[LauncherPrefGameDirViewController new] animated:YES];
         }
         return;
     }
-
+    
     [self actionEditProfile:PLProfiles.current.profiles.allValues[indexPath.row]];
 }
 
-#pragma mark Context Menu configuration
+#pragma mark - Context Menu configuration
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle != UITableViewCellEditingStyleDelete) return;
-
+    
     UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
     NSString *title = localize(@"preference.title.confirm", nil);
-    // reusing the delete runtime message
+    // Using the delete runtime message
     NSString *message = [NSString stringWithFormat:localize(@"preference.title.confirm.delete_runtime", nil), cell.textLabel.text];
     UIAlertController *confirmAlert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleActionSheet];
     confirmAlert.popoverPresentationController.sourceView = cell;
     confirmAlert.popoverPresentationController.sourceRect = cell.bounds;
     UIAlertAction *ok = [UIAlertAction actionWithTitle:localize(@"OK", nil) style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        // Retrieve profile to delete.
+        NSMutableDictionary *profile = PLProfiles.current.profiles[cell.textLabel.text];
+        // Delete associated modpack folder.
+        NSString *gameDir = profile[@"gameDir"];
+        if (gameDir && gameDir.length > 0) {
+            // Assume gameDir is relative to the general game directory.
+            NSString *baseDir = getPrefObject(@"general.game_directory");
+            if ([gameDir hasPrefix:@"./"]) {
+                gameDir = [gameDir substringFromIndex:2];
+            }
+            NSString *fullPath = [baseDir stringByAppendingPathComponent:gameDir];
+            NSError *error = nil;
+            if (![[NSFileManager defaultManager] removeItemAtPath:fullPath error:&error]) {
+                NSLog(@"Failed to delete modpack folder at %@: %@", fullPath, error);
+            }
+        }
+        
         [PLProfiles.current.profiles removeObjectForKey:cell.textLabel.text];
         if ([PLProfiles.current.selectedProfileName isEqualToString:cell.textLabel.text]) {
-            // The one being deleted is the selected one, switch to the random one now
-            PLProfiles.current.selectedProfileName = PLProfiles.current.profiles.allKeys[0];
+            PLProfiles.current.selectedProfileName = PLProfiles.current.profiles.allKeys.firstObject;
             [self.navigationController performSelector:@selector(reloadProfileList)];
         } else {
             [PLProfiles.current save];
@@ -252,9 +251,8 @@ typedef NS_ENUM(NSUInteger, LauncherProfilesTableSection) {
     [self presentViewController:confirmAlert animated:YES completion:nil];
 }
 
-- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (indexPath.section == kInstances || PLProfiles.current.profiles.count==1) {
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == kInstances || PLProfiles.current.profiles.count == 1) {
         return UITableViewCellEditingStyleNone;
     }
     return UITableViewCellEditingStyleDelete;
