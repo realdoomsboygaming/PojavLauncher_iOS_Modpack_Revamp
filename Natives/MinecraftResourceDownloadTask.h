d1573d6bab35aa41a3aa6bd2f248a532b5498e54
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-24 08:37:21 -0600

    Update MinecraftResourceDownloadTask.h
    Add new public method

--- Natives/MinecraftResourceDownloadTask.h
+++ Natives/MinecraftResourceDownloadTask.h
@@ -3,15 +3,29 @@
 @class ModpackAPI;
 
 @interface MinecraftResourceDownloadTask : NSObject
+
 @property NSProgress *progress, *textProgress;
 @property NSMutableArray *fileList, *progressList;
-@property NSMutableDictionary* metadata;
-@property(nonatomic, copy) void(^handleError)(void);
+@property NSMutableDictionary *metadata;
+@property (nonatomic, copy) void(^handleError)(void);
 
-- (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url size:(NSUInteger)size sha:(NSString *)sha altName:(NSString *)altName toPath:(NSString *)path;
+// Creates and returns a download task.
+- (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url
+                                            size:(NSUInteger)size
+                                             sha:(NSString *)sha
+                                         altName:(NSString *)altName
+                                           toPath:(NSString *)path;
+
+// Finishes the download with an error message.
 - (void)finishDownloadWithErrorString:(NSString *)error;
 
+// Downloads the entire version.
 - (void)downloadVersion:(NSDictionary *)version;
+
+// Downloads a modpack from the API.
 - (void)downloadModpackFromAPI:(ModpackAPI *)api detail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion;
+
+// public method to signal finalization of downloads
+- (void)finalizeDownloads;
 
 @end
