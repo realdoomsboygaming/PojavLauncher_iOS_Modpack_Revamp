7d72061316288fdc53b01bab57530689a49bafef
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-23 19:52:45 -0600

    Update ModpackUtils.h

--- Natives/installer/modpack/ModpackUtils.h
+++ Natives/installer/modpack/ModpackUtils.h
@@ -6,4 +6,6 @@
 + (void)archive:(UZKArchive *)archive extractDirectory:(NSString *)dir toPath:(NSString *)path error:(NSError **)error;
 + (NSDictionary *)infoForDependencies:(NSDictionary *)dependency;
 
++ (void)extractArchiveAtPath:(NSString *)archivePath toDestination:(NSString *)destPath completion:(void (^)(NSError *error))completion;
+
 @end
