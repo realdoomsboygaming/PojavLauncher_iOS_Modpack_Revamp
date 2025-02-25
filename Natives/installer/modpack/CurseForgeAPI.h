7cd4c27e10b84fae01bfb2ba3b7aa644d0646549
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-24 20:55:46 -0600

    Update CurseForgeAPI.h

--- Natives/installer/modpack/CurseForgeAPI.h
+++ Natives/installer/modpack/CurseForgeAPI.h
@@ -7,17 +7,9 @@ NS_ASSUME_NONNULL_BEGIN
 @interface CurseForgeAPI : ModpackAPI
 
 - (instancetype)initWithAPIKey:(NSString *)apiKey NS_DESIGNATED_INITIALIZER;
-
-- (void)searchModWithFilters:(NSDictionary *)searchFilters
-         previousPageResult:(NSMutableArray *)prevResult
-                 completion:(void (^ _Nonnull)(NSMutableArray * _Nullable results, NSError * _Nullable error))completion;
-
-- (void)loadDetailsOfMod:(NSMutableDictionary *)item
-              completion:(void (^ _Nonnull)(NSError * _Nullable error))completion;
-
-- (void)installModpackFromDetail:(NSDictionary *)modDetail
-                         atIndex:(NSUInteger)selectedVersion
-                      completion:(void (^ _Nonnull)(NSError * _Nullable error))completion;
+- (void)searchModWithFilters:(NSDictionary *)searchFilters previousPageResult:(NSMutableArray *)prevResult completion:(void (^ _Nonnull)(NSMutableArray * _Nullable results, NSError * _Nullable error))completion;
+- (void)loadDetailsOfMod:(NSMutableDictionary *)item completion:(void (^ _Nonnull)(NSError * _Nullable error))completion;
+- (void)installModpackFromDetail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion completion:(void (^ _Nonnull)(NSError * _Nullable error))completion;
 
 @property (nonatomic, weak, nullable) UIViewController *parentViewController;
 
