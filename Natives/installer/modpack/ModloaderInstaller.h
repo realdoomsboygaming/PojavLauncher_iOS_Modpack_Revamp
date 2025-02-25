05d453cdd671282e5ade17fef57d9caf39938ab3
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-25 14:37:48 -0600

    Update ModloaderInstaller.h

--- Natives/installer/modpack/ModloaderInstaller.h
+++ Natives/installer/modpack/ModloaderInstaller.h
@@ -28,6 +28,17 @@ NS_ASSUME_NONNULL_BEGIN
 + (nullable NSDictionary *)readInstallerInfoFromModpackDirectory:(NSString *)modpackDirectory
                                                           error:(NSError **)error;
 
+/**
+ Performs the modloader installation process by reading the installer file and then
+ initiating the download and install process based on the loader type. For Forge,
+ it uses CurseForgeAPI to auto-install; for Fabric, it presents the FabricInstallViewController.
+
+ @param modpackDirectory The path to the modpack directory.
+ @param vc The view controller from which to present any necessary UI (for Fabric).
+ */
++ (void)performModloaderInstallationForModpackDirectory:(NSString *)modpackDirectory
+                                   fromViewController:(UIViewController *)vc;
+
 @end
 
 NS_ASSUME_NONNULL_END
