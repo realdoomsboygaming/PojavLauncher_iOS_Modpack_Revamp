6982b97c1cf54c021191ab260279e38a0c215e04
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-25 09:41:33 -0600

    Update ModrinthAPI.m

--- Natives/installer/modpack/ModrinthAPI.m
+++ Natives/installer/modpack/ModrinthAPI.m
@@ -1,6 +1,7 @@
 #import "MinecraftResourceDownloadTask.h"
 #import "ModrinthAPI.h"
 #import "PLProfiles.h"
+#import "ModloaderInstaller.h"
 
 @implementation ModrinthAPI
 
@@ -58,14 +59,14 @@
     NSMutableArray<NSString *> *hashes = [NSMutableArray new];
     NSMutableArray<NSString *> *sizes = [NSMutableArray new];
     [response enumerateObjectsUsingBlock:
-  ^(NSDictionary *version, NSUInteger i, BOOL *stop) {
-        NSDictionary *file = [version[@"files"] firstObject];
-        mcNames[i] = [version[@"game_versions"] firstObject];
-        sizes[i] = file[@"size"];
-        urls[i] = file[@"url"];
-        NSDictionary *hashesMap = file[@"hashes"];
-        hashes[i] = hashesMap[@"sha1"] ?: [NSNull null];
-    }];
+     ^(NSDictionary *version, NSUInteger i, BOOL *stop) {
+         NSDictionary *file = [version[@"files"] firstObject];
+         mcNames[i] = [version[@"game_versions"] firstObject];
+         sizes[i] = file[@"size"];
+         urls[i] = file[@"url"];
+         NSDictionary *hashesMap = file[@"hashes"];
+         hashes[i] = hashesMap[@"sha1"] ?: [NSNull null];
+     }];
     item[@"versionNames"] = names;
     item[@"mcVersionNames"] = mcNames;
     item[@"versionSizes"] = sizes;
@@ -91,13 +92,14 @@
 
     downloader.progress.totalUnitCount = [indexDict[@"files"] count];
     for (NSDictionary *indexFile in indexDict[@"files"]) {
-/*
-        if ([indexFile[@"downloads"] count] > 1) {
-            [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Unhandled multiple files download %@", indexFile[@"downloads"]]];
-            return;
+        // Here we fix the URL extraction: if downloads is an array use firstObject; if a dictionary, use the "primary" key.
+        id downloads = indexFile[@"downloads"];
+        NSString *url = @"";
+        if ([downloads isKindOfClass:[NSArray class]]) {
+            url = [downloads firstObject];
+        } else if ([downloads isKindOfClass:[NSDictionary class]]) {
+            url = downloads[@"primary"];
         }
-*/
-        NSString *url = [indexFile[@"downloads"] firstObject];
         NSString *sha = indexFile[@"hashes"][@"sha1"];
         NSString *path = [destPath stringByAppendingPathComponent:indexFile[@"path"]];
         NSUInteger size = [indexFile[@"fileSize"] unsignedLongLongValue];
@@ -134,19 +136,74 @@
         NSURLSessionDownloadTask *task = [downloader createDownloadTask:depInfo[@"json"] size:0 sha:nil altName:nil toPath:jsonPath];
         [task resume];
     }
-    // TODO: automation for Forge
-
-    // Create profile
+    
+    // Create profile as before
     NSString *tmpIconPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"icon.png"];
     PLProfiles.current.profiles[indexDict[@"name"]] = @{
         @"gameDir": [NSString stringWithFormat:@"./custom_gamedir/%@", destPath.lastPathComponent],
         @"name": indexDict[@"name"],
         @"lastVersionId": depInfo[@"id"],
         @"icon": [NSString stringWithFormat:@"data:image/png;base64,%@",
-            [[NSData dataWithContentsOfFile:tmpIconPath]
-            base64EncodedStringWithOptions:0]]
+                   [[NSData dataWithContentsOfFile:tmpIconPath]
+                    base64EncodedStringWithOptions:0]]
     }.mutableCopy;
     PLProfiles.current.selectedProfileName = indexDict[@"name"];
+
+    // Write the mod loader installer file
+    // Determine loader info from manifest:
+    NSDictionary *minecraft = indexDict[@"minecraft"];
+    NSString *vanillaVersion = @"";
+    NSString *modLoaderId = @"";
+    NSString *modLoaderVersion = @"";
+    if (minecraft && [minecraft isKindOfClass:[NSDictionary class]]) {
+        vanillaVersion = minecraft[@"version"] ?: @"";
+        NSArray *modLoaders = minecraft[@"modLoaders"];
+        NSDictionary *primaryModLoader = nil;
+        if ([modLoaders isKindOfClass:[NSArray class]] && modLoaders.count > 0) {
+            for (NSDictionary *loader in modLoaders) {
+                if ([loader[@"primary"] boolValue]) {
+                    primaryModLoader = loader;
+                    break;
+                }
+            }
+            if (!primaryModLoader) { primaryModLoader = modLoaders[0]; }
+            NSString *rawId = primaryModLoader[@"id"] ?: @"";
+            NSRange dashRange = [rawId rangeOfString:@"-"];
+            if (dashRange.location != NSNotFound) {
+                NSString *loaderName = [rawId substringToIndex:dashRange.location];
+                NSString *loaderVer = [rawId substringFromIndex:(dashRange.location + 1)];
+                if ([loaderName isEqualToString:@"forge"]) {
+                    modLoaderId = @"forge";
+                    modLoaderVersion = loaderVer;
+                } else if ([loaderName isEqualToString:@"fabric"]) {
+                    modLoaderId = @"fabric";
+                    modLoaderVersion = [NSString stringWithFormat:@"fabric-loader-%@-%@", loaderVer, vanillaVersion];
+                } else {
+                    modLoaderId = loaderName;
+                    modLoaderVersion = loaderVer;
+                }
+            } else {
+                modLoaderId = rawId;
+                modLoaderVersion = rawId;
+            }
+        }
+    }
+    NSString *finalVersionString = @"";
+    if ([modLoaderId isEqualToString:@"forge"]) {
+        finalVersionString = [NSString stringWithFormat:@"%@-forge-%@", vanillaVersion, modLoaderVersion];
+    } else if ([modLoaderId isEqualToString:@"fabric"]) {
+        finalVersionString = modLoaderVersion;
+    } else {
+        finalVersionString = [NSString stringWithFormat:@"%@ | %@", vanillaVersion, modLoaderId];
+    }
+    NSLog(@"downloader: Determined version string: %@", finalVersionString);
+    
+    // Call the ModloaderInstaller to write installer info to the modpack folder.
+    NSError *installerError = nil;
+    BOOL successInstaller = [ModloaderInstaller createInstallerFileInModpackDirectory:destPath withVersionString:finalVersionString loaderType:modLoaderId error:&installerError];
+    if (!successInstaller) {
+        NSLog(@"[ModloaderInstaller] Failed to create installer file: %@", installerError.localizedDescription);
+    }
 }
 
 @end
