4cf939ce1f8398c293801b3357a007e9d1d345ae
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-27 13:15:27 -0600

    Update LauncherNavigationController.h

--- Natives/LauncherNavigationController.h
+++ Natives/LauncherNavigationController.h
@@ -1,12 +1,17 @@
 #import <UIKit/UIKit.h>
 
-extern NSMutableArray<NSDictionary *> *localVersionList, *remoteVersionList;
+extern NSMutableArray<NSDictionary *> *localVersionList;
+extern NSMutableArray<NSDictionary *> *remoteVersionList;
 
 @interface LauncherNavigationController : UINavigationController
-@property (nonatomic, strong) UIProgressView *progressViewMain, *progressViewSub;
+
+@property (nonatomic, strong) UIProgressView *progressViewMain;
+@property (nonatomic, strong) UIProgressView *progressViewSub;
 @property (nonatomic, strong) UILabel *progressText;
 @property (nonatomic, strong) UIButton *buttonInstall;
+
 - (void)enterModInstallerWithPath:(NSString *)path hitEnterAfterWindowShown:(BOOL)hitEnter;
 - (void)fetchLocalVersionList;
 - (void)setInteractionEnabled:(BOOL)enable forDownloading:(BOOL)downloading;
+
 @end
