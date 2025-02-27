d29ffb22cdd08fdc75fc41ca4e1ee47568807d93
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-27 12:44:47 -0600

    Update FabricInstallViewController.h

--- Natives/installer/FabricInstallViewController.h
+++ Natives/installer/FabricInstallViewController.h
@@ -2,4 +2,5 @@
 #import "PLPrefTableViewController.h"
 
 @interface FabricInstallViewController : PLPrefTableViewController
+@property (nonatomic, strong) NSString *modpackDirectory; // Path to modpack directory for modloader installer info
 @end
