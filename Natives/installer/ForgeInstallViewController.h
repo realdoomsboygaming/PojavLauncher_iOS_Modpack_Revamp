c5d6c39ae8e7a8433612472aec223414c39892a9
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-27 12:44:20 -0600

    Update ForgeInstallViewController.h

--- Natives/installer/ForgeInstallViewController.h
+++ Natives/installer/ForgeInstallViewController.h
@@ -1,4 +1,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ForgeInstallViewController : UITableViewController
+@property (nonatomic, strong) NSString *modpackDirectory; // Path to modpack directory for modloader installer info
 @end
