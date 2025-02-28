0d0998269b476945903d1bb69dafc641efa952e4
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-28 09:16:04 -0600

    Update ModpackAPI.h

--- Natives/installer/modpack/ModpackAPI.h
+++ Natives/installer/modpack/ModpackAPI.h
@@ -1,21 +1,19 @@
 #import <Foundation/Foundation.h>
 #import "ModpackUtils.h"
 #import "UnzipKit.h"
-
 @class MinecraftResourceDownloadTask;
+NS_ASSUME_NONNULL_BEGIN
 
 @interface ModpackAPI : NSObject
-@property(nonatomic) NSString *baseURL;
-@property(nonatomic) NSError *lastError;
-@property(nonatomic) BOOL reachedLastPage;
-
+@property (nonatomic, strong) NSString *baseURL;
+@property (nonatomic, strong) NSError *lastError;
+@property (nonatomic) BOOL reachedLastPage;
 - (instancetype)initWithURL:(NSString *)url;
 - (NSMutableArray *)searchModWithFilters:(NSDictionary *)filters previousPageResult:(NSMutableArray *)prevResult;
 - (void)loadDetailsOfMod:(NSMutableDictionary *)item;
-
 - (void)installModpackFromDetail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion;
 - (void)downloader:(MinecraftResourceDownloadTask *)downloader submitDownloadTasksFromPackage:(NSString *)packagePath toPath:(NSString *)destPath;
-
 - (id)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params;
-
 @end
+
+NS_ASSUME_NONNULL_END
