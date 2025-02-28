b170d617789472009eaa0ea332d82ced96401c84
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-28 09:43:52 -0600

    Update ModpackInstallViewController.h

--- Natives/installer/ModpackInstallViewController.h
+++ Natives/installer/ModpackInstallViewController.h
@@ -1,4 +1,20 @@
 #import <UIKit/UIKit.h>
+#import "modpack/ModrinthAPI.h"
+#import "modpack/CurseForgeAPI.h"
 
-@interface ModpackInstallViewController : UITableViewController<UISearchResultsUpdating>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ModpackInstallViewController : UITableViewController <UISearchResultsUpdating, UIContextMenuInteractionDelegate>
+
+@property (nonatomic, strong) UISearchController *searchController;
+@property (nonatomic, strong) UISegmentedControl *apiSegmentControl;
+@property (nonatomic, strong) NSMutableArray *list;
+@property (nonatomic, strong) NSMutableDictionary *filters;
+@property (nonatomic, strong) ModrinthAPI *modrinth;
+@property (nonatomic, strong) CurseForgeAPI *curseForge;
+
+- (void)updateSearchResults;
+
 @end
+
+NS_ASSUME_NONNULL_END
