b23b4ca9602b82915bd1ab0d943c00ce1919d391
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-02-28 09:41:12 -0600

    Update CurseForgeAPI.h

--- Natives/installer/modpack/CurseForgeAPI.h
+++ Natives/installer/modpack/CurseForgeAPI.h
@@ -1,8 +1,19 @@
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import "ModpackAPI.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CurseForgeAPI : ModpackAPI
+
+- (instancetype)initWithAPIKey:(NSString *)apiKey NS_DESIGNATED_INITIALIZER;
+- (void)searchModWithFilters:(NSDictionary *)searchFilters previousPageResult:(NSMutableArray *)prevResult completion:(void (^ _Nonnull)(NSMutableArray * _Nullable results, NSError * _Nullable error))completion;
+- (void)loadDetailsOfMod:(NSMutableDictionary *)item completion:(void (^ _Nonnull)(NSError * _Nullable error))completion;
+- (void)installModpackFromDetail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion completion:(void (^ _Nonnull)(NSError * _Nullable error))completion;
+- (void)autoInstallForge:(NSString *)vanillaVer loaderVersion:(NSString *)forgeVer;
+
+@property (nonatomic, weak, nullable) UIViewController *parentViewController;
+
 @end
 
 NS_ASSUME_NONNULL_END
