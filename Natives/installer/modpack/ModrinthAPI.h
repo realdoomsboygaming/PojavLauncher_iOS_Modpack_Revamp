a87e07a72fb0df3d31998021128558b19faae728
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-02 16:05:26 -0600

    Update ModrinthAPI.h

--- Natives/installer/modpack/ModrinthAPI.h
+++ Natives/installer/modpack/ModrinthAPI.h
@@ -1,11 +1,15 @@
 #import <Foundation/Foundation.h>
 #import "ModpackAPI.h"
-
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ModrinthAPI : ModpackAPI
 
 - (NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, id> *)searchFilters previousPageResult:(NSMutableArray *)modrinthSearchResult;
+
+// Synchronous version (for use on a background thread)
+- (void)loadDetailsOfModSync:(NSMutableDictionary *)item;
+
+// Asynchronous version (calls completion on the main thread)
 - (void)loadDetailsOfMod:(NSMutableDictionary *)item completion:(void (^)(NSError *error))completion;
 
 @end
