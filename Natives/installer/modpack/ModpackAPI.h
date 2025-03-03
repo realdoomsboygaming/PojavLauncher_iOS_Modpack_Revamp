b4cc0099b7d6587e876c5b9d1bdf57c2a32f2247
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-03 08:53:12 -0600

    Update ModpackAPI.h

--- Natives/installer/modpack/ModpackAPI.h
+++ Natives/installer/modpack/ModpackAPI.h
@@ -12,13 +12,9 @@
 - (instancetype)initWithURL:(NSString *)url;
 - (NSMutableArray *)searchModWithFilters:(NSDictionary *)filters previousPageResult:(NSMutableArray *)prevResult;
 - (void)loadDetailsOfMod:(NSMutableDictionary *)item;
-
 - (void)installModpackFromDetail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion;
 - (void)downloader:(MinecraftResourceDownloadTask *)downloader submitDownloadTasksFromPackage:(NSString *)packagePath toPath:(NSString *)destPath;
-
 - (id)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params;
-
-// NEW: Asynchronous variant to prevent blocking.
 - (void)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params completion:(void (^)(id result, NSError *error))completion;
 
 @end
